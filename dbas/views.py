1f5f32e227e89acf6a0cdd1bf397ed3827b4d98e
Tobias Krauthoff <tobias@ubuntu-server.fritz.box>
2015-04-17 02:37:52 +0200

    New color theme; inheritancements of pt

--- dbas/views.py
+++ dbas/views.py
@@ -14,21 +14,17 @@ class Prototype(object):
 
     @view_config(route_name='main_page', renderer='templates/home.pt')
     def main_page(self):
-        return dict(title='Main',project='dbas')
+        return dict(title='Main',project='DBAS')
 
     @view_config(route_name='main_login', renderer='templates/login.pt')
     def main_login(self):
-        return dict(title='Login',project='dbas')
+        return dict(title='Login',project='DBAS')
 
     @view_config(route_name='main_contact', renderer='templates/contact.pt')
     def main_contact(self):
-        return dict(title='Contact',project='dbas')
+        return dict(title='Contact',project='DBAS')
 
-    #@view_config(context=HTTPNotFound, renderer='404.pt')
-    #def not_found(self):
-    #    request.response.status = 404
-    #    return {}
     @notfound_view_config(renderer='templates/404.pt')
     def notfound(self):
         self.request.response.status = 404
-        return dict(title='Error',project='dbas')
+        return dict(title='Error',project='DBAS')
