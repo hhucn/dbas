093ed0240adba81f50abb81fa2372908b4187219
Tobias <krauthoff@cs.uni-duesseldorf.de>
2015-05-26 23:42:51 +0200

    email sharing is now ready

--- dbas/static/js/news.js
+++ dbas/static/js/news.js
@@ -4,10 +4,29 @@ $(document).ready(function () {
 	'use strict';
 
 	$(".share-mail").click(function (event) {
-		var textarray, container;
+		var textarray, container, subject, message, date, author;
 		container = $(this).parents(".newscontainer");
+
+		// get the subject
+		textarray = container.html().split('<span class="font-semi-bold">');
+		textarray = textarray[1].split('</span>');
+		subject = "DBAS: " + textarray[0];
+
+		// get the date
+		textarray = container.html().split('<h3><p>');
+		textarray = textarray[1].split('</p>');
+		date = textarray[0];
+
+		// get the author
+		textarray = container.html().split('Author: ');
+		textarray = textarray[1].split('</h4>');
+		author = textarray[0];
+
+		// get the message
 		textarray = container.html().split('<br>');
-		alert("Share with mail:\n" + textarray[1]);
+		message="News from " + date + ", by " + author + ": " + textarray[1];
+
+		window.location.href = "mailto:user@example.com?subject=" + subject + "&body=" + message
 	});
 	
 	$(".share-twitter").click(function (event) {
