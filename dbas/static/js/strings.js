83e1ed2159b832442b8900bfac926586c6a281bf
Tobias Krauthoff <tobias.krauthoff@hhu.de>
2015-10-21 08:47:50 +0200

    mainpage

--- dbas/static/js/strings.js
+++ dbas/static/js/strings.js
@@ -4,8 +4,8 @@
  * @copyright Krauthoff 2015
  */
 
-var mainpage = 'http://localhost:4284/';
-//var mainpage = 'https://dbas.cs.uni-duesseldorf.de/';
+//var mainpage = 'http://localhost:4284/';
+var mainpage = 'https://dbas.cs.uni-duesseldorf.de/';
 
 _t = function(id){
 	var this_id, value = 'unknown identifier';
