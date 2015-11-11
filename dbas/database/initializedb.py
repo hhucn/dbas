8472b55fb7177ef5793733c61c4a2cd6a2e4ee88
Tobias Krauthoff <tobias.krauthoff@hhu.de>
2015-11-11 19:55:27 +0100

    few fixes for i18n

--- dbas/database/initializedb.py
+++ dbas/database/initializedb.py
@@ -179,10 +179,19 @@ def setupNewsDatabase():
 				  date='15.10.2015',
 				  author='Tobias Krauthoff',
 				  news='Since today we can switch between different topics :) Unfortunately this feature is not really tested ;-)')
+	news31 = News(title='Stable release',
+				  date='10.11.2015',
+				  author='Tobias Krauthoff',
+				  news='After two weeks of debugging, a first and stable version is online. Now we can start with the interessing things!')
+	news32 = News(title='Design Update',
+				  date='11.11.2015',
+				  author='Tobias Krauthoff',
+				  news='Today we released a new material-oriented design. Enjoy it!')
 
 	news_array = [news01, news02, news03, news04, news05, news06, news07, news08, news09, news10,
 	              news11, news12, news13, news14, news15, news16, news29, news18, news19, news20,
-	              news21, news22, news23, news24, news25, news26, news27, news28, news30]
+	              news21, news22, news23, news24, news25, news26, news27, news28, news30, news31,
+	              news32]
 	DBNewsSession.add_all(news_array[::-1])
 	DBNewsSession.flush()
 
