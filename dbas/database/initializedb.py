3ae43ad7dad05489b6a69a1548ad291804039e9a
Tobias Krauthoff <tobias.krauthoff@hhu.de>
2015-11-24 09:48:03 +0100

    new news

--- dbas/database/initializedb.py
+++ dbas/database/initializedb.py
@@ -191,7 +191,7 @@ def setupNewsDatabase():
 				  date='16.11.2015',
 				  author='Tobias Krauthoff',
 				  news='Bootstraping is one of the main challenges in discussion. Therefore we have a two-step process for this task!')
-	news33 = News(title='Breadcrumbs',
+	news34 = News(title='Breadcrumbs',
 				  date='24.11.2015',
 				  author='Tobias Krauthoff',
 				  news='Now we have a breadcrumbs with shortcuts for every step in our discussion. This feature will be im improved soon!')
@@ -199,7 +199,7 @@ def setupNewsDatabase():
 	news_array = [news01, news02, news03, news04, news05, news06, news07, news08, news09, news10,
 	              news11, news12, news13, news14, news15, news16, news29, news18, news19, news20,
 	              news21, news22, news23, news24, news25, news26, news27, news28, news30, news31,
-	              news32, news33]
+	              news32, news33, news34]
 	DBNewsSession.add_all(news_array[::-1])
 	DBNewsSession.flush()
 
