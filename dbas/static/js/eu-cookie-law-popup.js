e0be196bfe1699b122ddc90e3232a4b3039914d5
Tobias Krauthoff <tobias.krauthoff@hhu.de>
2015-12-08 11:05:10 +0100

    minor changes due to piwik

--- dbas/static/js/eu-cookie-law-popup.js
+++ dbas/static/js/eu-cookie-law-popup.js
@@ -123,9 +123,9 @@ $.fn.euCookieLawPopup = (function() {
 				'<div class="eupopup-body">' + _self.params.popupText + '</div>' +
 				'<div class="eupopup-buttons">' +
 					'<a href="#" class="eupopup-button eupopup-button_1">' + _self.params.buttonContinueTitle + '</a>' +
-					//'<a href="' + _self.params.cookiePolicyUrl + '"' +
-					//	(_self.params.buttonLearnmoreOpenInNewWindow ? ' target=_blank ' : '') +
-					//	' class="eupopup-button eupopup-button_2">' + _self.params.buttonLearnmoreTitle + '</a>' +
+					'<a href="' + _self.params.cookiePolicyUrl + '"' +
+						(_self.params.buttonLearnmoreOpenInNewWindow ? ' target=_blank ' : '') +
+						' class="eupopup-button eupopup-button_2">' + _self.params.buttonLearnmoreTitle + '</a>' +
 					'<div class="clearfix"></div>' +
 				'</div>' +
 				//'<a href="#" class="eupopup-closebutton">x</a>' +
