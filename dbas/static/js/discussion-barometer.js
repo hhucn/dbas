b107ee5af5bc58345b2509dccf934d1ab58aa2b8
Teresa Uebber <Teresa.Uebber@hhu.de>
2016-03-05 18:00:08 +0100

    added chart.js to static/js, inserted in templates/content.pt; modified discussion-barometer.js

--- dbas/static/js/discussion-barometer.js
+++ dbas/static/js/discussion-barometer.js
@@ -19,5 +19,18 @@ function DiscussionBarometer(){
 		 * 4. using chartjs.org with doghnut
 		 * 5. displayConfirmationDialogWithoutCancelAndFunction with html as text and suitable header
 		 */
+
+		$(document).ready(function() {
+			$.ajax({
+				url: "ajax_get_user_with_same_opinion",
+				type: 'GET',
+				dataType: 'json',
+				data: {uid:0, is_argument: false},
+				async: true
+			}).done(function () {
+			}).fail(function () {
+			});
+		});
+
 	};
 }
