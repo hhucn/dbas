66ff6603a8e95f06b7d7fa5aa33488eda6c49908
Teresa Uebber <Teresa.Uebber@hhu.de>
2016-03-07 21:28:35 +0100

    processing ajax-request data

--- dbas/static/js/discussion-barometer.js
+++ dbas/static/js/discussion-barometer.js
@@ -20,17 +20,36 @@ function DiscussionBarometer(){
 		 * 5. displayConfirmationDialogWithoutCancelAndFunction with html as text and suitable header
 		 */
 
-		$(document).ready(function() {
-			$.ajax({
-				url: "ajax_get_user_with_same_opinion",
-				type: 'GET',
-				dataType: 'json',
-				data: {uid:0, is_argument: false},
-				async: true
-			}).done(function () {
-			}).fail(function () {
-			});
-		});
+		getDictionary();
+	};
 
+	/**
+	 * sends Ajax-Request to Server
+	 * saves answer in array
+	 * */
+	getDictionary = function() {
+		$.ajax({
+			url: "ajax_get_user_with_same_opinion",
+			type: 'GET',
+			dataType: 'json',
+			data: {
+				uid: 0,
+				is_argument: false
+			},
+			async: true
+		}).done(function (data) {
+			var obj = JSON.parse(data);
+			var error = obj.error;
+			var votes = [];
+			var i = 0;
+			$.each(obj.votes, function () {
+				votes[i] = [];
+				$.each(this, function (key, value) {
+					votes[i].push("Key: " + key + ", Value:" + value);
+				});
+				i++;
+			});
+		}).fail(function () {
+		});
 	};
 }
