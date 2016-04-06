5e3fcff2d6d37cdcb7242445b3053554fc58697e
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-04-06 14:09:06 +0200

    i18n of session expired popup

--- dbas/static/js/discussion/discussion-helper.js
+++ dbas/static/js/discussion/discussion-helper.js
@@ -236,7 +236,7 @@ function Helper() {
 		innerHeight -= list.outerHeight(true);
 		innerHeight -= this.getPaddingOfElement(bar);
 		innerHeight -= this.getPaddingOfElement(list);
-		return innerHeight;
+		return innerHeight - 10;
 	};
 
 	this.getPaddingOfElement = function (element){
