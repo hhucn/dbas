29725c8ae299cef911c0066befeb53f58d65181e
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-04-07 15:19:56 +0200

    Update startup script

--- run.sh
+++ run.sh
@@ -5,7 +5,16 @@
 # Start postgresql, update all requirements and
 # start development server of D-BAS
 
-echo "Starting local D-BAS instance..."
-sudo systemctl start postgresql.service
+service=postgresql.service
+active=`systemctl is-active $service`
+
+if ! [ $active == "active"  ]
+then
+    echo "Starting Postgresql..."
+    sudo systemctl start $service
+fi
+
 pip install -U -r requirements.txt
+
+echo "Starting local D-BAS instance..."
 pserve development.ini --reload
