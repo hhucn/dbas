399d5715e63d6c4dc3742ad34149f64d7f4dc4aa
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-04-08 07:44:16 +0200

    sigma improvement

--- dbas/input_validator.py
+++ dbas/input_validator.py
@@ -7,3 +7,31 @@ class Validator:
 	@staticmethod
 	def do_something():
 		return 1
+
+	@staticmethod
+	def save_params_in_session(session, user_arg_uid, sys_arg_uid, mood, reaction):
+		session['user_arg_uid'] = user_arg_uid
+		session['sys_arg_uid'] = sys_arg_uid
+		session['mood'] = mood
+		session['reaction'] = reaction
+
+	@staticmethod
+	def validate_params_with_session(session, user_arg_uid, sys_arg_uid, mood, reaction):
+		"""
+
+		:param session:
+		:param user_arg_uid:
+		:param sys_arg_uid:
+		:param mood:
+		:param reaction:
+		:return:
+		"""
+		if session['user_arg_uid'] != user_arg_uid:
+			return 1
+		if session['sys_arg_uid'] != sys_arg_uid:
+			return 2
+		if session['mood'] != mood:
+			return 3
+		if session['reaction'] != reaction:
+			return 4
+		return 0
