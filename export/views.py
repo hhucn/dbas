5e8c2b48933c910725f18c507f383d02ec3e185a
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-04-13 11:22:20 +0200

    new issue helper

--- export/views.py
+++ export/views.py
@@ -11,7 +11,7 @@ from pyramid.threadlocal import get_current_registry
 from dbas.lib import get_language
 from dbas.logger import logger
 from export.lib import get_dump, get_sigma_export
-from dbas.helper.query_helper import QueryHelper
+from dbas.helper.issue_helper import IssueHelper
 
 # =============================================================================
 # SERVICES - Define services for several actions of DBAS
@@ -34,8 +34,7 @@ sigma = Service(name='export_sigma',
 def get_database_dump(request):
 	logger('- - - - - - - - - - - -', '- - - - - - - - - - - -', '- - - - - - - - - - - -')
 	logger('Export', 'get_database_dump', 'main')
-	_qh = QueryHelper()
-	issue = _qh.get_issue_id(request)
+	issue = IssueHelper.get_issue_id(request)
 	ui_locales = get_language(request, get_current_registry())
 
 	return_dict = get_dump(issue, ui_locales)
@@ -46,8 +45,7 @@ def get_database_dump(request):
 def get_sigma_dump(request):
 	logger('- - - - - - - - - - - -', '- - - - - - - - - - - -', '- - - - - - - - - - - -')
 	logger('Export', 'get_sigma_dump', 'main')
-	_qh = QueryHelper()
-	issue = _qh.get_issue_id(request)
+	issue = IssueHelper.get_issue_id(request)
 
 	return_dict = get_sigma_export(issue)
 	return json.dumps(return_dict, True)
