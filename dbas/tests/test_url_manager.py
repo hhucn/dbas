c801829cdd9e5900a1ef766aceb8b8ae27d619cc
Teresa Uebber <Teresa.Uebber@hhu.de>
2016-05-26 21:40:27 +0200

    Add comments for unit-tests

--- dbas/tests/test_url_manager.py
+++ dbas/tests/test_url_manager.py
@@ -21,48 +21,69 @@ class UrlManagerTests(unittest.TestCase):
                              for_api=False,
                              history='Abc123/()')
         # TODO Hier würde ich mir doch ein paar Kommentare wünschen, warum du das hier gerade so testest...
+        # Test whether backslash is added, if application_url does not end with it.
         self.assertEqual(url1.url, 'application_url/')
+        # Test whether empty String is added, if application_url ends with backslash.
         self.assertEqual(url2.url, 'application_url/')
 
+        # Test if 'discuss/' is attached in discussion_url.
         self.assertEqual(url1.discussion_url, 'application_url/discuss/')
 
+        # Test if 'api/' is assigned to variable 'api_url'.
         self.assertEquals(url1.api_url, 'api/')
 
+        # Test empty string.
         self.assertEqual(url1.slug, '')
+        # Test string.
         self.assertEqual(url2.slug, 'cat-or-dog')
 
+        # Verify variable 'for_api' is assigned to expected boolean value.
         self.assertEqual(url1.for_api, True)
         self.assertEqual(url2.for_api, False)
 
+        # Test empty string.
         self.assertEquals(url1.history, '')
+        # Test string.
         self.assertEquals(url2.history, 'Abc123/()')
 
+        # Test whether function returns 'None'.
         self.assertEqual(url1.__init__('application_url/'), None)
 
     def test_get_404(self):
         url = self._makeOne('application_url')
 
-        responseArrayTrue = url.get_404(params=['discuss', '123', '.(%', 'A1('],
-                                        is_param_error=True)
-        self.assertEqual(responseArrayTrue, 'application_url/404/discuss/123/.(%/A1(?param_error=true')
+        response_array_true = url.get_404(params=['discuss', '123', '.(%', 'A1('],
+                                          is_param_error=True)
+        # If an element of array 'params' is not empty:
+        # verify that, if there is no character '?' in array 'params' and value of 'is_param_error' is 'True',
+        # the elements are put together to an url, separated with backslash and '?param_error=true' is attached in url.
+        self.assertEqual(response_array_true, 'application_url/404/discuss/123/.(%/A1(?param_error=true')
 
-        responseArrayFalse = url.get_404(params=['discuss', '123', '.(%', 'A1('],
-                                         is_param_error=False)
-        self.assertEqual(responseArrayFalse, 'application_url/404/discuss/123/.(%/A1(')
+        response_array_false = url.get_404(params=['discuss', '123', '.(%', 'A1('],
+                                           is_param_error=False)
+        # Verify that, if there is no character '?' in array 'params' and value of 'is_param_error' is 'False', the url is returned.
+        self.assertEqual(response_array_false, 'application_url/404/discuss/123/.(%/A1(')
 
-        responseQuestionMarkTrue = url.get_404(params=['?discuss'],
-                                               is_param_error=True)
-        self.assertEqual(responseQuestionMarkTrue, 'application_url/404/?discuss&param_error=true')
+        response_question_mark_true = url.get_404(params=['?discuss'],
+                                                  is_param_error=True)
+        # Verify that, if there is a character '?' in array 'params' and value of 'is_param_error' is 'True',
+        # '&param_error=true' is attached in url.
+        self.assertEqual(response_question_mark_true, 'application_url/404/?discuss&param_error=true')
 
-        responseQuestionMarkFalse = url.get_404(params=['?discuss'],
-                                                is_param_error=False)
-        self.assertEqual(responseQuestionMarkFalse, 'application_url/404/?discuss')
+        response_question_mark_false = url.get_404(params=['?discuss'],
+                                                   is_param_error=False)
+        # Verify that, if there is a character '?' in array 'params' and value of 'is_param_error' is 'False', the url is returned.
+        self.assertEqual(response_question_mark_false, 'application_url/404/?discuss')
 
-        responseEmptyTrue = url.get_404(params=['discuss', '', '123'],
-                                        is_param_error=True)
-        self.assertEqual(responseEmptyTrue, 'application_url/404/discuss/123?param_error=true')
+        response_empty_true = url.get_404(params=['discuss', '', '123'],
+                                          is_param_error=True)
+        # Verify that if an element of array 'params' is empty and value of 'is_param_error' is 'True',
+        # there is no backslash between empty element and next element, '?param_error=true' is attached in url.
+        self.assertEqual(response_empty_true, 'application_url/404/discuss/123?param_error=true')
 
-        responseEmptyFalse = url.get_404(params=['discuss', '', '123'],
-                                         is_param_error=False)
-        self.assertEqual(responseEmptyFalse, 'application_url/404/discuss/123')
+        response_empty_false = url.get_404(params=['discuss', '', '123'],
+                                           is_param_error=False)
+        # Verify that if an element of array 'params' is empty and value of 'is_param_error' is 'False',
+        # there is no backslash between empty element and next element.
+        self.assertEqual(response_empty_false, 'application_url/404/discuss/123')
 
