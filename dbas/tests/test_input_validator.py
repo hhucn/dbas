d7ec71b49ec2067b36b93fa2bdb0d87527fa5c33
Teresa Uebber <Teresa.Uebber@hhu.de>
2016-06-06 11:12:36 +0200

    Add unit test for check_reaction

--- dbas/tests/test_input_validator.py
+++ dbas/tests/test_input_validator.py
@@ -1,4 +1,6 @@
 import unittest
+from dbas.database import DBDiscussionSession
+from dbas.database.discussion_model import Argument, VoteArgument
 
 
 class InputValidatorTests(unittest.TestCase):
@@ -7,3 +9,23 @@ class InputValidatorTests(unittest.TestCase):
     def _getTargetClass():
         from dbas.input_validator import Validator
         return Validator
+
+    def _makeOne(self, *args, **kw):
+        return self._getTargetClass()(*args, **kw)
+
+    def test_check_reaction(self):
+        url = self._makeOne()
+
+        self.assertEqual(url.check_reaction(attacked_arg_uid=1,
+                                            attacking_arg_uid=1,
+                                            relation='end',
+                                            is_history=False), False);
+
+        self.assertEqual(url.check_reaction(attacked_arg_uid=1,
+                                            attacking_arg_uid=0,
+                                            relation='end',
+                                            is_history=False), True);
+
+
+
+
