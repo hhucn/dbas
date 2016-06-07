871e71c0435941d150609e380809fc3acfc9695e
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-07 09:39:17 +0200

    Beautify output; print connect-string to console

--- docker/core/docker_init.sh
+++ docker/core/docker_init.sh
@@ -8,16 +8,22 @@ do
 done
 echo "$(date) - db is ready, starting server"
 
-echo "Deploying D-BAS..."
+printf "\n# Deploying D-BAS...\n"
 python setup.py develop
 
-echo "# Seeding discussion database..."
-initialize_discussion_sql development.ini
+printf "\n# Seeding discussion database...\n"
+initialize_discussion_sql docker.ini
 
-echo "# Seeding news database..."
-initialize_news_sql development.ini
+printf "\n# Seeding news database...\n"
+initialize_news_sql docker.ini
 
-echo "# Starting integrated web server -- for development use only!"
-pserve development.ini --reload
+ip=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
+
+printf "\n###################################################"
+printf "\n# Connect to this client via http://$ip/ "
+printf "\n###################################################\n"
+
+printf "\n# Starting integrated web server -- for development use only!\n"
+pserve docker.ini --reload
 
 echo "I feel a disturbance in the Force..."
