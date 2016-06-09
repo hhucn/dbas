dcf085f76b8b770264d0da4aeb23e1ffc62cc1c2
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-06-09 12:08:18 +0200

    added graph component

---
+++ graph/views.py
@@ -0,0 +1,36 @@
+"""
+Introducing an graph manager.
+
+.. codeauthor:: Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de
+"""
+
+import json
+import dbas.helper.issue_helper as IssueHelper
+
+from cornice import Service
+
+from dbas.logger import logger
+from graph.lib import get_sigma_data
+
+# =============================================================================
+# SERVICES - Define services for several actions of DBAS
+# =============================================================================
+
+sigma_graph = Service(name='sigma',
+                      path='/sigma',
+                      description="Sigma Dump")
+
+
+# =============================================================================
+# GRAPH-RELATED REQUESTS
+# =============================================================================
+
+
+@sigma_graph.get()
+def get_sigma_dump(request):
+	logger('- - - - - - - - - - - -', '- - - - - - - - - - - -', '- - - - - - - - - - - -')
+	logger('Graph', 'simap', 'main')
+	issue = IssueHelper.get_issue_id(request)
+
+	return_dict = get_sigma_data(issue)
+	return json.dumps(return_dict, True)
