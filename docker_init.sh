4b50ee0e438e62ac92ec828bb1739c36471e49b5
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-14 10:18:12 +0200

    Loop pserve to prevent container dying

--- docker_init.sh
+++ docker_init.sh
@@ -1,36 +1,48 @@
 #!/usr/bin/env bash
 
-# check db is ready
-while ! ((>/dev/tcp/db/5432) &>/dev/null)
-do
-  echo "$(date) - waiting for db"
-  sleep 1
+while true; do
+
+    # check db is ready
+    while ! ((>/dev/tcp/db/5432) &>/dev/null)
+    do
+        echo "$(date) - waiting for db"
+        sleep 1
+    done
+    echo "$(date) - db is ready, starting server"
+
+    printf "\n# Deploying D-BAS...\n"
+    python setup.py develop
+
+    printf "\n# Seeding discussion database...\n"
+    initialize_discussion_sql docker.ini
+
+    printf "\n# Seeding news database...\n"
+    initialize_news_sql docker.ini
+
+    ip=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
+
+    printf "\n###################################################"
+    printf "\n# Connect to this client via http://$ip/ "
+    printf "\n###################################################\n"
+
+    printf "\n# Starting integrated web server -- for development use only!\n"
+    pserve docker.ini --reload
+
+    echo ""
+    echo "      --------------------------------------"
+    echo "     < I feel a disturbance in the Force... >"
+    echo "      --------------------------------------"
+    echo "        /"
+    echo "       /"
+    echo "      /"
+    echo "__.-._"
+    echo "'-._\"7'"
+    echo " /'.-c"
+    echo " |  /T"
+    echo "_)_/LI"
+    echo ""
+    echo "Fix your errors!"
+
+    sleep 3
+
 done
-echo "$(date) - db is ready, starting server"
-
-printf "\n# Deploying D-BAS...\n"
-python setup.py develop
-
-printf "\n# Seeding discussion database...\n"
-initialize_discussion_sql docker.ini
-
-printf "\n# Seeding news database...\n"
-initialize_news_sql docker.ini
-
-ip=`ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
-
-printf "\n###################################################"
-printf "\n# Connect to this client via http://$ip/ "
-printf "\n###################################################\n"
-
-printf "\n# Starting integrated web server -- for development use only!\n"
-pserve docker.ini --reload
-
-echo "I feel a disturbance in the Force..."
-echo ""
-echo "__.-._"
-echo "'-._\"7'"
-echo " /'.-c"
-echo " |  /T"
-echo "_)_/LI"
-echo ""
