36ce5f438a55aea003f3df262fc1353e826995c0
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-22 23:41:55 +0200

    Remove parameter from function call

--- dbas/tests/test_input_validator.py
+++ dbas/tests/test_input_validator.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from dbas import DBDiscussionSession
 from dbas.helper.tests_helper import add_settings_to_appconfig
 
-settings = add_settings_to_appconfig("development.ini")
+settings = add_settings_to_appconfig()
 
 
 class InputValidatorTests(unittest.TestCase):
