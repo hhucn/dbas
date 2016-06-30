64386d4801e668145072bb93a87ccf80e674bb58
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-06-30 09:37:43 +0200

    Fix test

--- dbas/tests/test_password_handler.py
+++ dbas/tests/test_password_handler.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config
 from dbas import password_handler, DBDiscussionSession
 from dbas.helper.tests_helper import add_settings_to_appconfig
 
-settings = add_settings_to_appconfig("development.ini")
+settings = add_settings_to_appconfig()
 
 DBDiscussionSession.configure(bind=engine_from_config(settings, 'sqlalchemy-discussion.'))
 
@@ -13,5 +13,5 @@ DBDiscussionSession.configure(bind=engine_from_config(settings, 'sqlalchemy-disc
 class PasswordHandlerTests(unittest.TestCase):
 
     def test_get_rnd_passwd(self):
-        self.assertEqual(len(password_handler.get_rnd_passwd()), 12);
+        self.assertEqual(len(password_handler.get_rnd_passwd()), 10)
 
