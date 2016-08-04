424d575ba9e581812534ed8e17510c0cd0afe81f
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-08-04 15:57:37 +0200

    Style fix for #141

--- dbas/static/js/discussion/discussion-guihandler.js
+++ dbas/static/js/discussion/discussion-guihandler.js
@@ -132,6 +132,7 @@ function GuiHandler() {
 		var sidebarGraphTackWrapper = $('#' + sidebarTackGraphWrapperId);
 		var main = new Main();
 		var tacked_sidebar = 'tacked_graph_sidebar';
+		var header = $('#graph-view-container-header');
 		
 		this.setImageActive($('#' + displayStyleIconGuidedId));
 		this.setImageActive($('#' + displayStyleIconIslandId));
@@ -147,7 +148,7 @@ function GuiHandler() {
 		// height
 		var innerHeight = new Helper().getMaxSizeOfGraphViewContainer();
 		graphViewContainer.attr('style', 'height: ' + innerHeight + 'px; margin-left: 2em; margin-right: 2em; margin-bottom: 1em;');
-		innerHeight -= $('#graph-view-container-header').outerHeight(true) + 20;
+		innerHeight -= header.outerHeight(true) + 20;
 		graphViewContainerSpace.attr('style', 'height: ' + innerHeight + 'px; margin-left: 0.5em; margin-right: 0.5em; width: 95%');
 		new DiscussionGraph().showGraph();
 		main.setSidebarStyle(
@@ -167,7 +168,7 @@ function GuiHandler() {
 			$('#' + sidebarTackGraphId),
 			sidebarGraphTackWrapper,
 			tacked_sidebar,
-			$('#graph-view-container-header').outerHeight(true));
+			header.outerHeight(true));
 	};
 
 	/**
