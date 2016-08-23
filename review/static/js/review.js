4f3f7b7a4c02708011a62fb32ab84e022bc0919a
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-08-23 20:28:21 +0200

    JS for review

---
+++ review/static/js/review.js
@@ -0,0 +1,30 @@
+/**
+ * @author Tobias Krauthoff
+ * @email krauthoff@cs.uni-duesseldorf.de
+ */
+
+$(document).ready(function () {
+	var tabs = $('#review-tabs');
+	var id;
+	$.each(tabs.find('a'), function(){
+		$(this).click(function () {
+			id = $(this).attr('href');
+			hideAll();
+			$(id).show();
+		});
+	});
+	
+	// show first
+	hideAll();
+	id = tabs.find('a:first').attr('href');
+	$(id).show();
+});
+
+function hideAll(){
+	var tabs = $('#review-tabs');
+	var id;
+	$.each(tabs.find('a'), function(){
+		id = $(this).attr('href');
+		$(id).hide();
+	});
+}
