889ccbccbf84a9c4c51b44bcf9003cc06820a06b
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-09-30 15:19:03 +0200

    Update admin

--- admin/lib.py
+++ admin/lib.py
@@ -1,6 +1,6 @@
 # Common library for Admin Component
 #
-# @author Tobias Krautho66
+# @author Tobias Krauthoff
 # @email krautho66@cs.uni-duesseldorf.de
 
 from random import randint
@@ -42,6 +42,29 @@ table_mapper = {
     'RevokedContent'.lower(): {'table': RevokedContent, 'name': 'RevokedContent'}
 }
 
+google_colors = [
+    ['#f44336', '#ffebee', '#ffcdd2', '#ef9a9a', '#e57373', '#ef5350', '#f44336', '#e53935', '#d32f2f', '#c62828', '#b71c1c', '#ff8a80', '#ff5252', '#ff1744', '#d50000'],  # red
+    ['#e91e63', '#fce4ec', '#f8bbd0', '#f48fb1', '#f06292', '#ec407a', '#e91e63', '#d81b60', '#c2185b', '#ad1457', '#880e4f', '#ff80ab', '#ff4081', '#f50057', '#c51162'],  # pink
+    ['#9c27b0', '#f3e5f5', '#e1bee7', '#ce93d8', '#ba68c8', '#ab47bc', '#9c27b0', '#8e24aa', '#7b1fa2', '#6a1b9a', '#4a148c', '#ea80fc', '#e040fb', '#d500f9', '#aa00ff'],  # purple
+    ['#673ab7', '#ede7f6', '#d1c4e9', '#b39ddb', '#9575cd', '#7e57c2', '#673ab7', '#5e35b1', '#512da8', '#4527a0', '#311b92', '#b388ff', '#7c4dff', '#651fff', '#6200ea'],  # deep purple
+    ['#3f51b5', '#e8eaf6', '#c5cae9', '#9fa8da', '#7986cb', '#5c6bc0', '#3f51b5', '#3949ab', '#303f9f', '#283593', '#1a237e', '#8c9eff', '#536dfe', '#3d5afe', '#304ffe'],  # indigo
+    ['#2196f3', '#e3f2fd', '#bbdefb', '#90caf9', '#64b5f6', '#42a5f5', '#2196f3', '#1e88e5', '#1976d2', '#1565c0', '#0d47a1', '#82b1ff', '#448aff', '#2979ff', '#2962ff'],  # blue
+    ['#03a9f4', '#e1f5fe', '#b3e5fc', '#81d4fa', '#4fc3f7', '#29b6f6', '#03a9f4', '#039be5', '#0288d1', '#0277bd', '#01579b', '#80d8ff', '#40c4ff', '#00b0ff', '#0091ea'],  # light blue
+    ['#00bcd4', '#e0f7fa', '#b2ebf2', '#80deea', '#4dd0e1', '#26c6da', '#00bcd4', '#00acc1', '#0097a7', '#00838f', '#006064', '#84ffff', '#18ffff', '#00e5ff', '#00b8d4'],  # cyan
+    ['#009688', '#e0f2f1', '#b2dfdb', '#80cbc4', '#4db6ac', '#26a69a', '#009688', '#00897b', '#00796b', '#00695c', '#004d40', '#a7ffeb', '#64ffda', '#1de9b6', '#00bfa5'],  # teal
+    ['#4caf50', '#e8f5e9', '#c8e6c9', '#a5d6a7', '#81c784', '#66bb6a', '#4caf50', '#43a047', '#388e3c', '#2e7d32', '#1b5e20', '#b9f6ca', '#69f0ae', '#00e676', '#00c853'],  # green
+    ['#8bc34a', '#f1f8e9', '#dcedc8', '#c5e1a5', '#aed581', '#9ccc65', '#8bc34a', '#7cb342', '#689f38', '#558b2f', '#33691e', '#ccff90', '#b2ff59', '#76ff03', '#64dd17'],  # light green
+    ['#cddc39', '#f9fbe7', '#f0f4c3', '#e6ee9c', '#dce775', '#d4e157', '#cddc39', '#c0ca33', '#afb42b', '#9e9d24', '#827717', '#f4ff81', '#eeff41', '#c6ff00', '#aeea00'],  # lime
+    ['#ffeb3b', '#fffde7', '#fff9c4', '#fff59d', '#fff176', '#ffee58', '#ffeb3b', '#fdd835', '#fbc02d', '#f9a825', '#f57f17', '#ffff8d', '#ffff00', '#ffea00', '#ffd600'],  # yellow
+    ['#ffc107', '#fff8e1', '#ffecb3', '#ffe082', '#ffd54f', '#ffca28', '#ffc107', '#ffb300', '#ffa000', '#ff8f00', '#ff6f00', '#ffe57f', '#ffd740', '#ffc400', '#ffab00'],  # amber
+    ['#ff9800', '#fff3e0', '#ffe0b2', '#ffcc80', '#ffb74d', '#ffa726', '#ff9800', '#fb8c00', '#f57c00', '#ef6c00', '#e65100', '#ffd180', '#ffab40', '#ff9100', '#ff6d00'],  # orange
+    ['#ff5722', '#fbe9e7', '#ffccbc', '#ffab91', '#ff8a65', '#ff7043', '#ff5722', '#f4511e', '#e64a19', '#d84315', '#bf360c', '#ff9e80', '#ff6e40', '#ff3d00', '#dd2c00'],  # deep orange
+    ['#795548', '#efebe9', '#d7ccc8', '#bcaaa4', '#a1887f', '#8d6e63', '#795548', '#6d4c41', '#5d4037', '#4e342e', '#3e2723'],  # brown
+    ['#9e9e9e', '#fafafa', '#f5f5f5', '#eeeeee', '#e0e0e0', '#bdbdbd', '#9e9e9e', '#757575', '#616161', '#424242', '#212121'],  # grey
+    ['#607d8b', '#eceff1', '#cfd8dc', '#b0bec5', '#90a4ae', '#78909c', '#607d8b', '#546e7a', '#455a64', '#37474f', '#263238'],  # blue grey
+    ['#000000'],  # black
+    ['#ffffff']]  # white
+
 
 def get_dashboard_infos(main_page):
     """
@@ -50,24 +73,71 @@ def get_dashboard_infos(main_page):
     :return:
     """
     logger('AdminLib', 'get_dashboard_infos', 'main')
-    return_dict = list()
+    return_list = list()
 
-    tmp = list()
-    for key in table_mapper:
-        db_elements = DBDiscussionSession.query(table_mapper[key]['table']).all()
-        tmp.append(__get_dash_dict(len(db_elements), table_mapper[key]['name'], main_page + table_mapper[key]['name'], __get_random_color()))
+    general = list()
+    general.append(__get_dash_dict(len(DBDiscussionSession.query(Issue).all()), 'Issue', main_page + 'Issue'))
+    general.append(__get_dash_dict(len(DBDiscussionSession.query(Language).all()), 'Language', main_page + 'Language'))
+    general.append(__get_dash_dict(len(DBDiscussionSession.query(History).all()), 'History', main_page + 'History'))
 
-    row = list()
-    for index, el in enumerate(tmp):
-        if len(row) % 4 == 0:
-            return_dict.append(row)
-            row = list()
-        row.append(el)
+    users = list()
+    users.append(__get_dash_dict(len(DBDiscussionSession.query(Group).all()), 'Group', main_page + 'Group'))
+    users.append(__get_dash_dict(len(DBDiscussionSession.query(User).all()), 'User', main_page + 'User'))
+    users.append(__get_dash_dict(len(DBDiscussionSession.query(Settings).all()), 'Settings', main_page + 'Settings'))
+    users.append(__get_dash_dict(len(DBDiscussionSession.query(Message).all()), 'Message ', main_page + 'Message'))
 
-    if len(row) > 0:
-        return_dict.append(row)
+    content = list()
+    content.append(__get_dash_dict(len(DBDiscussionSession.query(Statement).all()), 'Statement', main_page + 'Statement'))
+    content.append(__get_dash_dict(len(DBDiscussionSession.query(TextVersion).all()), 'TextVersion', main_page + 'TextVersion'))
+    content.append(__get_dash_dict(len(DBDiscussionSession.query(StatementReferences).all()), 'StatementReferences', main_page + 'StatementReferences'))
+    content.append(__get_dash_dict(len(DBDiscussionSession.query(PremiseGroup).all()), 'PremiseGroup', main_page + 'PremiseGroup'))
+    content.append(__get_dash_dict(len(DBDiscussionSession.query(Premise).all()), 'Premise', main_page + 'Premise'))
+    content.append(__get_dash_dict(len(DBDiscussionSession.query(Argument).all()), 'Argument ', main_page + 'Argument'))
 
-    return return_dict
+    voting = list()
+    voting.append(__get_dash_dict(len(DBDiscussionSession.query(VoteArgument).all()), 'VoteArgument', main_page + 'VoteArgument'))
+    voting.append(__get_dash_dict(len(DBDiscussionSession.query(VoteStatement).all()), 'VoteStatement', main_page + 'VoteStatement'))
+    voting.append(__get_dash_dict(len(DBDiscussionSession.query(StatementSeenBy).all()), 'StatementSeenBy', main_page + 'StatementSeenBy'))
+    voting.append(__get_dash_dict(len(DBDiscussionSession.query(ArgumentSeenBy).all()), 'ArgumentSeenBy ', main_page + 'ArgumentSeenBy'))
+
+    reviews = list()
+    reviews.append(__get_dash_dict(len(DBDiscussionSession.query(ReviewDelete).all()), 'ReviewDelete', main_page + 'ReviewDelete'))
+    reviews.append(__get_dash_dict(len(DBDiscussionSession.query(ReviewEdit).all()), 'ReviewEdit', main_page + 'ReviewEdit'))
+    reviews.append(__get_dash_dict(len(DBDiscussionSession.query(ReviewEditValue).all()), 'ReviewEditValue', main_page + 'ReviewEditValue'))
+    reviews.append(__get_dash_dict(len(DBDiscussionSession.query(ReviewOptimization).all()), 'ReviewOptimization', main_page + 'ReviewOptimization'))
+    reviews.append(__get_dash_dict(len(DBDiscussionSession.query(ReviewDeleteReason).all()), 'ReviewDeleteReason ', main_page + 'ReviewDeleteReason'))
+
+    reviewer = list()
+    reviewer.append(__get_dash_dict(len(DBDiscussionSession.query(LastReviewerDelete).all()), 'LastReviewerDelete', main_page + 'LastReviewerDelete'))
+    reviewer.append(__get_dash_dict(len(DBDiscussionSession.query(LastReviewerEdit).all()), 'LastReviewerEdit', main_page + 'LastReviewerEdit'))
+    reviewer.append(__get_dash_dict(len(DBDiscussionSession.query(LastReviewerOptimization).all()), 'LastReviewerOptimization', main_page + 'LastReviewerOptimization'))
+
+    reputation = list()
+    reputation.append(__get_dash_dict(len(DBDiscussionSession.query(ReputationHistory).all()), 'ReputationHistory', main_page + 'ReputationHistory'))
+    reputation.append(__get_dash_dict(len(DBDiscussionSession.query(ReputationReason).all()), 'ReputationReason', main_page + 'ReputationReason'))
+    reputation.append(__get_dash_dict(len(DBDiscussionSession.query(OptimizationReviewLocks).all()), 'OptimizationReviewLocks', main_page + 'OptimizationReviewLocks'))
+    reputation.append(__get_dash_dict(len(DBDiscussionSession.query(ReviewCanceled).all()), 'ReviewCanceled', main_page + 'ReviewCanceled'))
+    reputation.append(__get_dash_dict(len(DBDiscussionSession.query(RevokedContent).all()), 'RevokedContent', main_page + 'RevokedContent'))
+
+    #from sqlalchemy import create_engine
+    #engine = create_engine("postgresql+psycopg2://dbas:SQL_2015&@localhost:5432/discussion?client_encoding=utf8")
+#
+    #from sqlalchemy import inspect
+    #inspector = inspect(engine)
+#
+    #for table_name in inspector.get_table_names():
+    #    for column in inspector.get_columns(table_name):
+    #        print("Column: %s" % column['name'])
+
+    return_list.append([{'name': 'General', 'content': general},
+                        {'name': 'Users', 'content': users},
+                        {'name': 'Content', 'content': content},
+                        {'name': 'Voting', 'content': voting}])
+    return_list.append([{'name': 'Reviews', 'content': reviews},
+                        {'name': 'Reviewer', 'content': reviewer},
+                        {'name': 'Reputation', 'content': reputation}])
+
+    return return_list
 
 
 def get_table_dict(table):
@@ -92,37 +162,54 @@ def get_table_dict(table):
     return_dict['name'] = table if db_elements else 'unknown table'
     return_dict['count'] = len(db_elements) if db_elements else '-1'
 
-    if db_elements:
-        head = list()
-        head.append('a')
-        head.append('a')
-        head.append('a')
-        return_dict['head'] = head
+    keys = list()
+    table = table_mapper[table.lower()]['table']
+    for key in table.__table__.columns.keys():
+        keys.append(key)
+
+    data = list()
+    for row in db_elements:
+        tmp = list()
+        for key in keys:
+            tmp.append(str(key))
+        data.append(tmp)
+
+    return_dict['head'] = keys
+    return_dict['row'] = data
 
     return return_dict
 
 
-def __get_dash_dict(count, name, href, color):
+def __get_dash_dict(count, name, href):
     """
 
     :param count:
     :param name:
     :param href:
-    :param color:
     :return:
     """
     return {
         'count': count,
         'name': name,
-        'href': href,
-        'style': 'background-color: ' + color
+        'href': href
         }
 
 
-def __get_random_color():
+def __get_random_color(index):
     """
 
+    :param index:
     :return:
     """
-    r = lambda: randint(100, 200)
-    return '#%02X%02X%02X' % (r(), r(), r())
+    color = randint(0, 15)
+    brightness = randint(1, 10)
+    # color = int(index / 10)
+    # brightness = index - color * 10
+    # brightness += 1
+    # if brightness > 10:
+    #    brightness = 1
+    #    color += 1
+    return google_colors[color][brightness]
+
+    # r = lambda: randint(100, 200)
+    # return '#%02X%02X%02X' % (r(), r(), r())
