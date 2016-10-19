981984f080d993daaca68980488e7d0ae9c2c250
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-10-19 09:16:41 +0200

    Tooltip color in barchart

--- dbas/static/js/d3/barometer.js
+++ dbas/static/js/d3/barometer.js
@@ -1,26 +1,24 @@
 // colors from https://www.google.com/design/spec/style/color.html#color-color-palette
-var colors = [
-	'#F44336', //  0 red
-	'#4CAF50', //  1 green
-	'#2196F3', //  2 blue
-	'#FFEB3B', //  3 yellow
-	'#673AB7', //  4 deep purple
-	'#03A9F4', //  5 light blue
-	'#FF5722', //  6 deep orange
-	'#607D8B', //  7 blue grey
-	'#E91E63', //  8 pink
-	'#3F51B5', //  9 indigo
-	'#00BCD4', // 10 cyan
-	'#8BC34A', // 11 light green
-	'#FFC107', // 12 amber
-	'#795548', // 13 brown
-	'#000000', // 14 black
-	'#9C27B0', // 15 purple
-	'#009688', // 16 teal
-	'#CDDC39', // 17 lime
-	'#FF9800', // 18 orange
-	'#9E9E9E'  // 19 grey
+var google_colors = [
+	//0          1          2          3          4          5          6          7          8          9          10         11         12         13         14
+	['#f44336', '#ffebee', '#ffcdd2', '#ef9a9a', '#e57373', '#ef5350', '#f44336', '#e53935', '#d32f2f', '#c62828', '#b71c1c', '#ff8a80', '#ff5252', '#ff1744', '#d50000'],  // red
+	['#4caf50', '#e8f5e9', '#c8e6c9', '#a5d6a7', '#81c784', '#66bb6a', '#4caf50', '#43a047', '#388e3c', '#2e7d32', '#1b5e20', '#b9f6ca', '#69f0ae', '#00e676', '#00c853'],  // green
+	['#2196f3', '#e3f2fd', '#bbdefb', '#90caf9', '#64b5f6', '#42a5f5', '#2196f3', '#1e88e5', '#1976d2', '#1565c0', '#0d47a1', '#82b1ff', '#448aff', '#2979ff', '#2962ff'],  // blue
+	['#ffeb3b', '#fffde7', '#fff9c4', '#fff59d', '#fff176', '#ffee58', '#ffeb3b', '#fdd835', '#fbc02d', '#f9a825', '#f57f17', '#ffff8d', '#ffff00', '#ffea00', '#ffd600'],  // yellow
+	['#673ab7', '#ede7f6', '#d1c4e9', '#b39ddb', '#9575cd', '#7e57c2', '#673ab7', '#5e35b1', '#512da8', '#4527a0', '#311b92', '#b388ff', '#7c4dff', '#651fff', '#6200ea'],  // deep purple
+	['#3f51b5', '#e8eaf6', '#c5cae9', '#9fa8da', '#7986cb', '#5c6bc0', '#3f51b5', '#3949ab', '#303f9f', '#283593', '#1a237e', '#8c9eff', '#536dfe', '#3d5afe', '#304ffe'],  // indigo
+	['#03a9f4', '#e1f5fe', '#b3e5fc', '#81d4fa', '#4fc3f7', '#29b6f6', '#03a9f4', '#039be5', '#0288d1', '#0277bd', '#01579b', '#80d8ff', '#40c4ff', '#00b0ff', '#0091ea'],  // light blue
+	['#00bcd4', '#e0f7fa', '#b2ebf2', '#80deea', '#4dd0e1', '#26c6da', '#00bcd4', '#00acc1', '#0097a7', '#00838f', '#006064', '#84ffff', '#18ffff', '#00e5ff', '#00b8d4'],  // cyan
+	['#e91e63', '#fce4ec', '#f8bbd0', '#f48fb1', '#f06292', '#ec407a', '#e91e63', '#d81b60', '#c2185b', '#ad1457', '#880e4f', '#ff80ab', '#ff4081', '#f50057', '#c51162'],  // pink
+	['#9c27b0', '#f3e5f5', '#e1bee7', '#ce93d8', '#ba68c8', '#ab47bc', '#9c27b0', '#8e24aa', '#7b1fa2', '#6a1b9a', '#4a148c', '#ea80fc', '#e040fb', '#d500f9', '#aa00ff'],  // purple
+	['#009688', '#e0f2f1', '#b2dfdb', '#80cbc4', '#4db6ac', '#26a69a', '#009688', '#00897b', '#00796b', '#00695c', '#004d40', '#a7ffeb', '#64ffda', '#1de9b6', '#00bfa5'],  // teal
+	['#8bc34a', '#f1f8e9', '#dcedc8', '#c5e1a5', '#aed581', '#9ccc65', '#8bc34a', '#7cb342', '#689f38', '#558b2f', '#33691e', '#ccff90', '#b2ff59', '#76ff03', '#64dd17'],  // light green
+	['#cddc39', '#f9fbe7', '#f0f4c3', '#e6ee9c', '#dce775', '#d4e157', '#cddc39', '#c0ca33', '#afb42b', '#9e9d24', '#827717', '#f4ff81', '#eeff41', '#c6ff00', '#aeea00'],  // lime
+	['#ffc107', '#fff8e1', '#ffecb3', '#ffe082', '#ffd54f', '#ffca28', '#ffc107', '#ffb300', '#ffa000', '#ff8f00', '#ff6f00', '#ffe57f', '#ffd740', '#ffc400', '#ffab00'],  // amber
+	['#ff9800', '#fff3e0', '#ffe0b2', '#ffcc80', '#ffb74d', '#ffa726', '#ff9800', '#fb8c00', '#f57c00', '#ef6c00', '#e65100', '#ffd180', '#ffab40', '#ff9100', '#ff6d00'],  // orange
+	['#ff5722', '#fbe9e7', '#ffccbc', '#ffab91', '#ff8a65', '#ff7043', '#ff5722', '#f4511e', '#e64a19', '#d84315', '#bf360c', '#ff9e80', '#ff6e40', '#ff3d00', '#dd2c00']   // deep orange
 	];
+
 
 function DiscussionBarometer(){
     'use strict';
@@ -238,7 +236,7 @@ function DiscussionBarometer(){
 			       x: function(d,i) {return i*barWidth + 55 + i*5;},
 			       // y: height - barLength, because d3 starts to draw in left upper corner
 			       y: function(d) {return height - (divideWrapperIfZero(d.usersNumber, d.seenBy) * height - 50);},
-			       fill: function (d, i) {return colors[i % colors.length];}});
+			       fill: function (d, i) {return getNormalColorFor(i);}});
 	}
 	
 	function divideWrapperIfZero(numerator, denominator){
@@ -258,6 +256,7 @@ function DiscussionBarometer(){
 		var barWidth = width / usersDict.length - 5;
 		var tooltipWith = width;
 		var tmp;
+		var _this = this;
 		barChartSvg.selectAll("rect").on("mouseover", function (d, index) {
 			var barLeft = index / 2 * barWidth + 70 + index * 5;
 			tmp = index;
@@ -298,11 +297,17 @@ function DiscussionBarometer(){
 			d.users.forEach(function (e) {
 				div.append('img').attr('src', e.avatar_url);
 			});
+			$(this).attr('fill', getDarkColorFor(index));
 		})
-		.on("mouseout", function () {
+		.on("mouseout", function (d, index) {
 			div.style("opacity", 0);
+			$(this).attr('fill', getNormalColorFor(index));
 		});
 	}
+	
+	function getNormalColorFor(index){ return google_colors[index % google_colors.length][0]; }
+	function getLightColorFor(index){ return google_colors[index % google_colors.length][2];	}
+	function getDarkColorFor(index){ return google_colors[index % google_colors.length][10];	}
 
 	/**
 	 * Create legend for chart.
@@ -312,7 +317,7 @@ function DiscussionBarometer(){
 	function createLegend(usersDict){
 		var div, label, element;
 		$.each(usersDict, function(key, value) {
-			div = $('<div>').attr('class', 'legendSymbolDiv').css('background-color', colors[key]);
+			div = $('<div>').attr('class', 'legendSymbolDiv').css('background-color', getNormalColorFor(key));
             label = $('<label>').attr('class', 'legendLabel').html(value.text);
 			element = $('<ul>').attr('class', 'legendUl').append(div).append(label);
 			$('#' + popupConfirmRowDialogId).find('.col-md-5').append(element);
@@ -366,10 +371,7 @@ function DiscussionBarometer(){
 	/**
 	 * Create pie chart.
 	 *
-	 * @param width
-	 * @param height
 	 * @param usersDict
-	 * @param barChartSvg
      */
 	function createPieChart(usersDict, pieChartSvg) {
 		var height = 400, width = 400,
@@ -447,7 +449,7 @@ function DiscussionBarometer(){
 			.data(pie(usersDict))
 			.enter().append("path")
 			.attr("fill", function (d, i) {
-				return colors[i];
+				return getNormalColorFor(i);
 			})
 			.attr("stroke", "gray")
 			.attr("d", innerCircle)
