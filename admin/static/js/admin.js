260b82447b859601d48a1819dd4b144968141da7
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-10-20 09:52:28 +0200

    Add input fields for edits in admin interface

--- admin/static/js/admin.js
+++ admin/static/js/admin.js
@@ -52,94 +52,112 @@ function AdminIndex(){
 	};
 }
 
-/**
- *
- * @param _this
- * @param elements_class
- * @param text_class
- */
-function activateElement(_this, elements_class, text_class){
-	var element = $(_this).parents('td:first').find('.' + elements_class);
-	element.removeClass('text-muted').addClass(text_class);
-	element.parent().css('pointer-events', '');
-}
-
-/**
- *
- * @param _this
- * @param elements_class
- * @param text_class
- */
-function deactivateElement(_this, elements_class, text_class){
-	var element = $(_this).parents('td:first').find('.' + elements_class);
-	element.addClass('text-muted').removeClass(text_class);
-	element.parent().css('pointer-events', 'none');
-}
-
-/**
- *
- * @param parent
- */
-function setAddClickEvent(parent){
-	parent.find('.add').each(function(){
-		$(this).click(function(){
-			console.log('todo create');
-		})
-	});
-}
-/**
- *
- * @param parent
- */
-function setEditClickEvent(parent){
-	parent.find('.pencil').each(function(){
-		$(this).click(function(){
-			var uid = $(this).parents('tr:first').find('td:first').text();
-			activateElement(this, 'floppy', 'text-success');
-			activateElement(this, 'square', 'text-danger');
-			console.log('todo edit ' + uid);
-		})
-	});
-}
-/**
- *
- * @param parent
- */
-function setDeleteClickEvent(parent){
-	parent.find('.trash').each(function(){
-		$(this).click(function(){
-			var uid = $(this).parents('tr:first').find('td:first').text();
-			console.log('todo delete ' + uid);
-		})
-	});
-}
-/**
- *
- * @param parent
- */
-function setSaveClickEvent(parent){
-	parent.find('.floppy').each(function(){
-		$(this).click(function(){
-			var uid = $(this).parents('tr:first').find('td:first').text();
-			deactivateElement(this, 'floppy', 'text-success');
-			deactivateElement(this, 'square', 'text-danger');
-			console.log('todo save ' + uid);
-		})
-	});
-}
-/**
- *
- * @param parent
- */
-function setCancelClickEvent(parent){
-	parent.find('.square').each(function(){
-		$(this).click(function(){
-			var uid = $(this).parents('tr:first').find('td:first').text();
-			deactivateElement(this, 'floppy', 'text-success');
-			deactivateElement(this, 'square', 'text-danger');
-			console.log('todo cancel ' + uid);
-		})
-	});
+function AdminGui() {
+	
+	/**
+	 *
+	 * @param _this
+	 * @param elements_class
+	 * @param text_class
+	 */
+	this.activateElement = function(_this, elements_class, text_class) {
+		var element = $(_this).parents('td:first').find('.' + elements_class);
+		element.removeClass('text-muted').addClass(text_class);
+		element.parent().css('pointer-events', '');
+	};
+	
+	/**
+	 *
+	 * @param _this
+	 * @param elements_class
+	 * @param text_class
+	 */
+	this.deactivateElement = function(_this, elements_class, text_class) {
+		var element = $(_this).parents('td:first').find('.' + elements_class);
+		element.addClass('text-muted').removeClass(text_class);
+		element.parent().css('pointer-events', 'none');
+	};
+	
+	/**
+	 *
+	 * @param parent
+	 */
+	this.setAddClickEvent = function(parent) {
+		parent.find('.add').each(function () {
+			$(this).click(function () {
+				console.log('todo create');
+			})
+		});
+	};
+	
+	/**
+	 *
+	 * @param parent
+	 */
+	this.setEditClickEvent = function(parent) {
+		var _this = this;
+		parent.find('.pencil').each(function () {
+			$(this).click(function () {
+				var parent = $(this).parents('tr:first');
+				var uid = parent.find('td:first').text();
+				_this.activateElement(this, 'floppy', 'text-success');
+				_this.activateElement(this, 'square', 'text-danger');
+				console.log('todo edit ' + uid);
+				parent.find('td:not(:last)').each(function(){
+					$(this).append($('<input>').attr({'class': 'form-control', 'placeholder': $(this).text()}).val($(this).text()));
+					$(this).find('span').hide();
+				});
+			})
+		});
+	};
+	
+	/**
+	 *
+	 * @param parent
+	 */
+	this.setDeleteClickEvent = function(parent) {
+		parent.find('.trash').each(function () {
+			$(this).click(function () {
+				var uid = $(this).parents('tr:first').find('td:first').text();
+				console.log('todo delete ' + uid);
+			})
+		});
+	};
+	
+	/**
+	 *
+	 * @param parent
+	 */
+	this.setSaveClickEvent = function(parent) {
+		var _this = this;
+		parent.find('.floppy').each(function () {
+			$(this).click(function () {
+				var uid = $(this).parents('tr:first').find('td:first').text();
+				_this.deactivateElement(this, 'floppy', 'text-success');
+				_this.deactivateElement(this, 'square', 'text-danger');
+				console.log('todo save ' + uid);
+			})
+		});
+	};
+	
+	/**
+	 *
+	 * @param parent
+	 */
+	this.setCancelClickEvent = function(parent) {
+		var _this = this;
+		parent.find('.square').each(function () {
+			$(this).click(function () {
+				var tmp = $(this).parents('tr:first');
+				var uid = tmp.find('td:first').text();
+				_this.deactivateElement(this, 'floppy', 'text-success');
+				_this.deactivateElement(this, 'square', 'text-danger');
+				console.log('todo cancel ' + uid);
+				tmp.find('input').remove();
+				tmp.find('span').show();
+			})
+		});
+	}
 }
 
 // main function
@@ -149,19 +167,20 @@ $(document).ready(function () {
 	});
 	
 	var data = $('#data');
+	var gui = new AdminGui();
 	
 	// events for edit
-	setEditClickEvent(data);
+	gui.setEditClickEvent(data);
 	
 	// events for delete
-	setCancelClickEvent(data);
+	gui.setDeleteClickEvent(data);
 	
 	// events for save
-	setSaveClickEvent(data);
+	gui.setSaveClickEvent(data);
 	
 	// events for cancel
-	setCancelClickEvent(data);
+	gui.setCancelClickEvent(data);
 	
 	// events for add
-	setAddClickEvent(data);
+	gui.setAddClickEvent(data);
 });
