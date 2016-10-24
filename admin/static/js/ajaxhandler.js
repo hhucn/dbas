e4d1e53ea15b6b5576e55715900fef19406ab598
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-10-24 15:12:28 +0200

    Smaller fixes; Selenium tests for admin interface

--- admin/static/js/ajaxhandler.js
+++ admin/static/js/ajaxhandler.js
@@ -2,55 +2,6 @@
  * Created by tobias on 20.10.16.
  */
 
-function AdminIndex(){
-	
-	/**
-	 *
-	 */
-	this.login = function () {
-		var csrfToken = $('#' + hiddenCSRFTokenId).val();
-		$('#admin-login-failed').hide();
-		$('#admin-login-failed-message').html('');
-		
-		var user = $('#admin-login-user').val();
-		var password = $('#admin-login-pw').val();
-		var url = window.location.href;
-		var keep_login = $('#admin-keep-login-box').prop('checked') ? 'true' : 'false';
-		$.ajax({
-			url: mainpage + 'ajax_user_login',
-			type: 'GET',
-			data: {
-				user: user,
-				password: password,
-				url: url,
-				keep_login: keep_login
-			},
-			dataType: 'json',
-			async: true,
-			headers: {'X-CSRF-Token': csrfToken}
-		}).done(function (data) { // display fetched data
-			try {
-				var jsonData = $.parseJSON(data);
-				if (jsonData.error.length != 0) {
-					$('#admin-login-failed').show();
-					$('#admin-login-failed-message').html(jsonData.error);
-				} else {
-					location.reload(true);
-				}
-			} catch(err){
-				//var htmlData = $.parseHTML(data);
-				var url = location.href;
-				if (url.indexOf('?session_expired=true') != -1)
-					url = url.substr(0, url.length - '?session_expired=true'.length);
-				location.href = url;
-			}
-		}).fail(function () { // display error message
-			setGlobalErrorHandler('Ohh', _t(requestFailed));
-			location.reload();
-		});
-	};
-}
-
 function AdminAjaxHandler(){
 	
 	/**
