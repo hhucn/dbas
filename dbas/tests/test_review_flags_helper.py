b26beffe834c86f6165a66529fc7067a00b95540
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-10-25 18:34:57 +0200

    Refactor flags function to use of new translator system

--- dbas/tests/test_review_flags_helper.py
+++ dbas/tests/test_review_flags_helper.py
@@ -4,12 +4,13 @@ from dbas.views import transaction
 import dbas.review.helper.flags as ReviewFlagHelper
 from dbas.database import DBDiscussionSession
 from dbas.helper.tests import add_settings_to_appconfig
-from dbas.strings.translator import Translator
+from dbas.strings.keywords import Keywords as _
 from sqlalchemy import engine_from_config
 from dbas.database.discussion_model import User, Argument, Statement
 
 settings = add_settings_to_appconfig()
 
+DBDiscussionSession.remove()
 DBDiscussionSession.configure(bind=engine_from_config(settings, 'sqlalchemy-discussion.'))
 
 
@@ -21,28 +22,26 @@ class TestReviewFlagHelper(unittest.TestCase):
         self.christian = DBDiscussionSession.query(User).filter_by(nickname='Christian').first()
 
     def test_flag_argument(self):
-        translator = Translator('en')
+        success, info, error = ReviewFlagHelper.flag_argument(0, 'reason', self.some_nick, Argument, transaction)
+        self.__assert_equal_text([[success, ''], [info, ''], [error, _.internalKeyError]])
 
-        success, info, error = ReviewFlagHelper.flag_argument(0, 'reason', self.some_nick, translator, Argument, transaction)
-        self.__assert_equal_text([[success, ''], [info, ''], [error, translator.internalKeyError]])
+        success, info, error = ReviewFlagHelper.flag_argument(4, 'reason', self.some_nick, Argument, transaction)
+        self.__assert_equal_text([[success, ''], [info, ''], [error, _.internalKeyError]])
 
-        success, info, error = ReviewFlagHelper.flag_argument(4, 'reason', self.some_nick, translator, Argument, transaction)
-        self.__assert_equal_text([[success, ''], [info, ''], [error, translator.internalKeyError]])
+        success, info, error = ReviewFlagHelper.flag_argument(4, 'reason', self.tobias, Argument, transaction)
+        self.__assert_equal_text([[success, ''], [info, ''], [error, _.internalKeyError]])
 
-        success, info, error = ReviewFlagHelper.flag_argument(4, 'reason', self.tobias, translator, Argument, transaction)
-        self.__assert_equal_text([[success, ''], [info, ''], [error, translator.internalKeyError]])
+        success, info, error = ReviewFlagHelper.flag_argument(0, 'reason', self.tobias, Argument, transaction)
+        self.__assert_equal_text([[success, ''], [info, ''], [error, _.internalKeyError]])
 
-        success, info, error = ReviewFlagHelper.flag_argument(0, 'reason', self.tobias, translator, Argument, transaction)
-        self.__assert_equal_text([[success, ''], [info, ''], [error, translator.internalKeyError]])
+        success, info, error = ReviewFlagHelper.flag_argument(4, 'optimization', self.tobias, Argument, transaction)
+        self.__assert_equal_text([[success, _.thxForFlagText], [info, ''], [error, '']])
 
-        success, info, error = ReviewFlagHelper.flag_argument(4, 'optimization', self.tobias, translator, Argument, transaction)
-        self.__assert_equal_text([[success, translator.thxForFlagText], [info, ''], [error, '']])
+        success, info, error = ReviewFlagHelper.flag_argument(4, 'optimization', self.tobias, Argument, transaction)
+        self.__assert_equal_text([[success, ''], [info, _.alreadyFlaggedByYou], [error, '']])
 
-        success, info, error = ReviewFlagHelper.flag_argument(4, 'optimization', self.tobias, translator, Argument, transaction)
-        self.__assert_equal_text([[success, ''], [info, translator.alreadyFlaggedByYou], [error, '']])
-
-        success, info, error = ReviewFlagHelper.flag_argument(4, 'optimization', self.christian, translator, Argument, transaction)
-        self.__assert_equal_text([[success, ''], [info, translator.alreadyFlaggedByOthers], [error, '']])
+        success, info, error = ReviewFlagHelper.flag_argument(4, 'optimization', self.christian, Argument, transaction)
+        self.__assert_equal_text([[success, ''], [info, _.alreadyFlaggedByOthers], [error, '']])
 
     def __assert_equal_text(self, values):
         for pair in values:
