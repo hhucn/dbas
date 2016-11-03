05dd3839d0e07fbd76dba0480a2f402c47d2cb21
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-11-03 15:31:16 +0100

    Split ajaxHandler

---
+++ dbas/static/js/ajax/news.js
@@ -0,0 +1,66 @@
+/**
+ * @author Tobias Krauthoff
+ * @email krauthoff@cs.uni-duesseldorf.de
+ */
+
+function AjaxNewsHandler(){
+	/**
+	 *
+	 */
+	this.ajaxGetNews = function () {
+		var csrf_token = $('#' + hiddenCSRFTokenId).val();
+		$.ajax({
+			url: 'ajax_get_news',
+			type: 'POST',
+			dataType: 'json',
+			async: true,
+			headers: {
+				'X-CSRF-Token': csrf_token
+			}
+		}).done(function ajaxGetNewsDone(data) {
+			new News().callbackIfDoneForGettingNews(data);
+		}).fail(function ajaxGetNewsFail() {
+			setGlobalErrorHandler(_t_discussion(ohsnap), _t_discussion(requestFailed));
+		});
+	};
+
+	/**
+	 *
+	 */
+	this.ajaxSendNews = function () {
+		var title = $('#' + writingNewNewsTitleId).val();
+		var text = $('#' + writingNewNewsTextId).val();
+
+		if (title.length == 0 || text.length < 10) {
+			$('#' + writingNewsFailedId).show();
+			$('#' + writingNewsFailedMessageId).text(_t(empty_news_input));
+			new Helper().delay(function(){
+				$('#' + writingNewsFailedId).fadeOut();
+				new Helper().delay(function(){
+					$('#' + writingNewsFailedMessageId).text('');
+				}, 2000);
+			}, 2000);
+			return;
+		} else {
+			$('#' + writingNewsFailedId).hide();
+			$('#' + writingNewsSuccessId).hide();
+		}
+
+		var csrf_token = $('#' + hiddenCSRFTokenId).val();
+		$.ajax({
+			url: 'ajax_send_news',
+			type: 'POST',
+			data: {title: title, text: text},
+			dataType: 'json',
+			async: true,
+			headers: {
+				'X-CSRF-Token': csrf_token
+			}
+		}).done(function ajaxSendNewsDone(data) {
+			new News().callbackIfDoneForSendingNews(data);
+		}).fail(function ajaxSendNewsFail() {
+			$('#' + writingNewsFailedId).show();
+			$('#' + writingNewsFailedMessageId).html(_t(internalError));
+		});
+	};
+}
---
+++ dbas/static/js/ajax/notification.js
@@ -0,0 +1,106 @@
+/**
+ * @author Tobias Krauthoff
+ * @email krauthoff@cs.uni-duesseldorf.de
+ */
+
+function AjaxNotificationHandler(){
+	/**
+    *
+    * @param id
+    * @param _this
+    */
+	this.sendAjaxForReadMessage = function(id, _this){
+		new Notifications().hideInfoSpaces();
+		var csrf_token = $('#' + hiddenCSRFTokenId).val();
+		$.ajax({
+			url: 'ajax_notification_read',
+			method: 'POST',
+			data: {
+				id: id
+			},
+			dataType: 'json',
+			headers: {'X-CSRF-Token': csrf_token}
+		}).done(function sendAjaxForReadMessageDone(data) {
+			var parsedData = $.parseJSON(data);
+			if (parsedData.error.length > 0) {
+				setGlobalErrorHandler(_t_discussion(ohsnap), parsedData.error);
+			} else {
+				var titletext = $(_this).text().replace(_t(neww).toLocaleUpperCase(), '').trim();
+				var spanEl = $('<span>').addClass('text-primary').text(titletext);
+				$(_this).empty().html(spanEl);
+				$('#collapse' + id).addClass('in');
+				new Notifications().setNewBadgeCounter(parsedData.unread_messages);
+			}
+		}).fail(function sendAjaxForReadMessageFail() {
+			setGlobalErrorHandler(_t_discussion(ohsnap), _t_discussion(requestFailed));
+		});
+	};
+
+	/**
+    *
+    * @param id
+    */
+	this.sendAjaxForDeleteMessage = function(id) {
+		new Notifications().hideInfoSpaces();
+		var csrf_token = $('#' + hiddenCSRFTokenId).val();
+		$.ajax({
+			url: 'ajax_notification_delete',
+			method: 'POST',
+			data: {
+				id: id
+			},
+			dataType: 'json',
+			headers: {'X-CSRF-Token': csrf_token}
+		}).done(function sendAjaxForDeleteMessageDone(data) {
+			var parsedData = $.parseJSON(data);
+			if (parsedData.success.length > 0) {
+				$('#' + id).remove();
+				new Notifications().setNewBadgeCounter(parsedData.unread_messages);
+				$('#total_in_counter').text(parsedData.total_in_messages);
+				$('#total_out_counter').text(parsedData.total_out_messages);
+				setGlobalSuccessHandler('', parsedData.success);
+			} else {
+				setGlobalErrorHandler(_t_discussion(ohsnap), parsedData.error);
+			}
+		}).fail(function sendAjaxForDeleteMessageFail() {
+			setGlobalErrorHandler(_t_discussion(ohsnap), _t_discussion(requestFailed));
+		});
+	};
+
+	/**
+    *
+    * @param recipient
+    */
+	this.sendNotification = function(recipient){
+		var title = $('#popup-writing-notification-title').val(),
+			text = $('#popup-writing-notification-text').val();
+		var csrf_token = $('#' + hiddenCSRFTokenId).val();
+
+		$('#popup-writing-notification-success').hide();
+		$('#popup-writing-notification-failed').hide();
+
+		$.ajax({
+			url: 'ajax_send_notification',
+			type: 'POST',
+			data: {title: title, text: text, recipient: recipient},
+			dataType: 'json',
+			async: true,
+			headers: {'X-CSRF-Token': csrf_token}
+		}).done(function ajaxSendNewsDone(data) {
+			var parsedData = $.parseJSON(data);
+			if (parsedData.error.length == 0) {
+				$('#popup-writing-notification-success').show();
+				$('#popup-writing-notification-success-message').text(_t(notificationWasSend));
+				var out_counter = $('#total_out_counter');
+				out_counter.text(' ' + (parseInt(out_counter.text()) + 1) + ' ');
+				new Notifications().appendMessageInOutbox(recipient, parsedData.recipient_avatar, title, text, parsedData.timestamp, parsedData.uid)
+			} else {
+				$('#popup-writing-notification-failed').show();
+				$('#popup-writing-notification-failed-message').html(parsedData.error);
+			}
+		}).fail(function ajaxSendNewsFail() {
+			$('#popup-writing-notification-failed').show();
+			$('#popup-writing-notification-failed-message').html(_t(internalError));
+		});
+	};
+}
