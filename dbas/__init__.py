8b6b273353769dcd38f3e6b9fdf866263e22da17
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-11-04 10:27:59 +0100

    Minor

--- dbas/__init__.py
+++ dbas/__init__.py
@@ -67,7 +67,6 @@ def main(global_config, **settings):
 
     # Include apps
     config.include('api', route_prefix='/api')
-    config.include('doj', route_prefix='/doj')
     config.include('graph', route_prefix='/graph')
     config.include('export', route_prefix='/export')
     config.include('admin', route_prefix='/admin')
