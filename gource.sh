547f1df95934a492f07127396aa9feb0bb8bdc98
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-11-10 16:05:45 +0100

    Minor

--- gource.sh
+++ gource.sh
@@ -13,8 +13,6 @@ gource \
     --background-colour 000000 \
     --font-size 22 \
     --title "Animated Version Control of D-BAS" \
-    --title "Animated Version Control of D-BAS" \
->>>>>>> 10d333b61cd36abbeeb79a3ebc8ad897357d9607
     --output-ppm-stream - \
     --output-framerate 25 \
     | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -b 65536K dbas_timelapse.mp4
