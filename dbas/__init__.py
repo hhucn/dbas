14b3e26decc5cff7bd00157912839ea45e2f8a0e
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-11-18 18:02:58 +0100

    Update configuration

--- dbas/__init__.py
+++ dbas/__init__.py
@@ -50,15 +50,15 @@ def main(global_config, **settings):
     set_cache_regions_from_settings(settings)
 
     # creating the configurator
-    settings = {'pyramid.default_locale_name': 'en',
-                'mail.host': 'imap.googlemail.com',
-                'mail.port': '465',
-                'mail.username': 'dbas.hhu@gmail.com',
-                'mail.password': 'orpcihtyuecxhoup',
-                'mail.ssl': 'True',
-                'mail.tls': 'False',
-                'mail.default_sender': 'dbas.hhu@gmail.com'
-                }
+    settings.update({'pyramid.default_locale_name': 'en',
+                     'mail.host': 'imap.googlemail.com',
+                     'mail.port': '465',
+                     'mail.username': 'dbas.hhu@gmail.com',
+                     'mail.password': 'orpcihtyuecxhoup',
+                     'mail.ssl': 'True',
+                     'mail.tls': 'False',
+                     'mail.default_sender': 'dbas.hhu@gmail.com'
+                     })
 
     # creating the configurator    cache_regions = set_cache_regions_from_settings
     config = Configurator(settings=settings,
