70e9ba4a5e3dbff7345245d3b924e2c53c2fd0e6
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-11-21 08:19:02 +0100

    Add DOJ request for D3

--- dbas/user_management.py
+++ dbas/user_management.py
@@ -614,11 +614,11 @@ def create_new_user(request, firstname, lastname, email, nickname, password, gen
     checknewuser = DBDiscussionSession.query(User).filter_by(nickname=nickname).first()
     if checknewuser:
         logger('UserManagement', 'create_new_user', 'New data was added with uid ' + str(checknewuser.uid))
-        success = _t.get(_.accountWasAdded)
+        success = _t.get(_.accountWasAdded).replace('XXX', nickname)
 
         # sending an email
         subject = _t.get(_.accountRegistration)
-        body = _t.get(_.accountWasRegistered)
+        body = _t.get(_.accountWasRegistered).replace('XXX', '"' + nickname + '"')
         email_helper.send_mail(request, subject, body, email, ui_locales)
         send_welcome_notification(checknewuser.uid)
 
