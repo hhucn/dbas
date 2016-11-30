b1f3e8e323b84521d50106c21715bc58da251e07
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-11-30 09:40:51 +0100

    Files for webtests

---
+++ dbas/tests/test_view_discussion_jump.py
@@ -0,0 +1,28 @@
+import unittest
+
+from pyramid import testing
+
+from dbas.database import DBDiscussionSession
+from dbas.helper.tests import add_settings_to_appconfig, verify_dictionary_of_view
+from sqlalchemy import engine_from_config
+
+settings = add_settings_to_appconfig()
+DBDiscussionSession.configure(bind=engine_from_config(settings, 'sqlalchemy-discussion.'))
+
+
+class DiscussionJumpViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+        self.config.include('pyramid_chameleon')
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_discussion_justifypage(self):
+        from dbas.views import discussion_jump as d
+
+        request = testing.DummyRequest()
+        response = d(request)
+        verify_dictionary_of_view(self, response)
+
+        # place for additional stuff
---
+++ dbas/tests/test_view_main_publications.py
@@ -0,0 +1,28 @@
+import unittest
+
+from pyramid import testing
+
+from dbas.database import DBDiscussionSession
+from dbas.helper.tests import add_settings_to_appconfig, verify_dictionary_of_view
+from sqlalchemy import engine_from_config
+
+settings = add_settings_to_appconfig()
+DBDiscussionSession.configure(bind=engine_from_config(settings, 'sqlalchemy-discussion.'))
+
+
+class MainPublicationsViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+        self.config.include('pyramid_chameleon')
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_main_publications_page(self):
+        from dbas.views import main_publications as d
+
+        request = testing.DummyRequest()
+        response = d(request)
+        verify_dictionary_of_view(self, response)
+
+        # place for additional stuff
