d9cceee18a60d0cab24aa4c0e02b7c54a085e787
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2016-12-15 09:52:23 +0100

    Update script for #215

--- dbas/static/minify.sh
+++ dbas/static/minify.sh
@@ -7,15 +7,15 @@
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # files which are minifed (has to be in the same order as paths!
-files=($path"/js/compiled/main.min.js"
-       $path"/js/compiled/main_discussion.min.js"
-       $path"/js/compiled/main_ajax.min.js"
-       $path"/js/compiled/main_review.min.js")
+files=($path"/js/min/main.min.js"
+       $path"/js/min/main_ajax.min.js"
+       $path"/js/min/main_discussion.min.js"
+       $path"/js/min/main_review.min.js")
 
 # path for files, which should be minimized
 paths=($path"/js/*.js"
-       $path"/js/discussion/*.js"
        $path"/js/ajax/*.js"
+       $path"/js/discussion/*.js"
        $path"/js/review/*.js")
 
 # path for files, which should be appended for the final file
@@ -23,32 +23,38 @@ appends=($path"/js/min/main_ajax.min.js"
          $path"/js/min/main.min.js"
          $path"/js/min/main_discussion.min.js"
          $path"/js/min/main_review.min.js"
+         $path"/js/bootstrap/*"
+         $path"/js/d3/*"
+         $path"/js/jquery/*"
+         $path"/js/libs/*"
+         $path"/js/socketio/*"
          )
 
-final_file=$path"/js/compiled/dbas.min.js"
-rm -f ${final_file};
 
 mkdir -p $path"/js/min"
+
+final_file=$path"/js/min/dbas.min.js"
+rm -f ${files[i]}/*;
+rm -f ${final_file};
+
+old_size=$(find . -name "*.js" -ls | awk '{total += $7} END {print total}')
+old_size=$(expr $old_size / 1000)
+file_count=$(find $path/js -type f | wc -l)
 
 # minimize
 length=${#files[@]}
 max_iter=$(expr $length - 1)
 for i in `seq 0 $max_iter`;
     do
-    rm -f ${files[i]}
     touch ${files[i]}
-
-    old_size=$(find . -name "*.css" -ls | awk '{total += $7} END {print total}')
-    old_size=$(expr $old_size / 1000)
-    echo "Start compressing files with size" $old_size"K"
 
     for file in ${paths[i]}
         do if [ "$file" != "${files[i]}" ]; then
             if [[ "$file" != *'.min.js' ]]; then
-                echo "  Compressing" $file;
-    	        java -jar ~/node_modules/yuicompressor/build/2-2.4.8.jar $file >> ${files[i]};
+                echo "Compressing" $file;
+    	        java -jar ~/node_modules/yuicompressor/build/yuicompressor-2.4.8.jar $file >> ${files[i]};
     	    else
-                echo "  Appending  " $file;
+                echo "Appending  " $file;
     	        cat  $file >> ${files[i]};
     	    fi
     	fi
@@ -56,12 +62,12 @@ for i in `seq 0 $max_iter`;
 
     new_size=$(du ${files[i]} | cut -f 1)
     save=$(expr $old_size - $new_size)
-    echo "Finished ${files[i]} with" $new_size"K"
-    echo "Saved" $save"K"
     echo ""
 done
 
 # append
+length=${#appends[@]}
+max_iter=$(expr $length - 1)
 for j in `seq 0 $max_iter`;
     do
     for file in ${appends[j]}
@@ -78,4 +84,5 @@ for i in `seq 0 $max_iter`;
 done
 
 echo ""
-echo "Final size: " $(du ${final_file} | cut -f 1)"K"
+echo "Compressed "${file_count}" files with size" $old_size"K"
+echo "Final size:" $(du ${final_file} | cut -f 1)"K"
