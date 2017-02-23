ba596002394ad28d848cf2cd636db7aec6a1b437
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-02-23 15:25:06 +0100

    Update #354

--- analyzeDiscussion.py
+++ analyzeDiscussion.py
@@ -247,9 +247,9 @@ print('')
 
 
 quit_count = 2
-quit_counters = [300, 600, 900]
+quit_counters = [300, 600, 900]  # 5, 10, 15 Minutes
 for quit_counter in quit_counters:
-    print('User Specific History Quits (Quit Count > {}, Quit Counter = {}s):'.format(quit_count, quit_counter))
+    print('User Specific History Quits (Quit Count >= {}, Quit Counter = {}s):'.format(quit_count + 1, quit_counter))
     history = {}
     for user in db_users:
         history[user.nickname] = session.query(History).filter_by(author_uid=user.uid).all()
@@ -258,10 +258,9 @@ for quit_counter in quit_counters:
     for user_nickname in history:
         for i in range(0, len(history[user_nickname]) - 1):
             step = history[user_nickname][i].path
-            if abs((history[user_nickname][i+1].timestamp - history[user_nickname][i].timestamp).seconds) > quit_counter \
+            if abs((history[user_nickname][i + 1].timestamp - history[user_nickname][i].timestamp).seconds) > quit_counter \
                     and not any(x in step for x in ['admin', 'rss', 'contact', 'finish', 'settings', 'finish', 'imprint', 'news'])\
-                    and len(step) > 2:  # 10 Minutes
-                # print('  - User {} quit after {}'.format(user_nickname, step))
+                    and len(step) > 2:
                 quit_after[step] = quit_after[step] + 1 if step in quit_after else 1
     sorted_quit_after = sorted(quit_after.items(), key=lambda x: x[1])
     for step in sorted_quit_after:
