223396cb6369a86b2b021e9e57debcb361ef681c
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-03-02 17:49:17 +0100

    First test to establish connection to dabas

--- api/tests/test_lib.py
+++ api/tests/test_lib.py
@@ -32,11 +32,3 @@ def test_merge_dicts():
     assert merge_dicts(a, a) == a
     assert merge_dicts(a, None) is None
     assert merge_dicts(a, b) == c
-
-
-def test_as_json():
-    correct = {"foo": "bar"}
-    broken_input = as_json(None)
-    assert as_json(correct) == '{"foo": "bar"}'
-    assert "status" in broken_input and "error" in broken_input
-    assert as_json(1) == "1"
--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -4,10 +4,12 @@ Testing the routes of the API.
 .. codeauthor:: Christian Meter <meter@cs.uni-duesseldorf.de>
 """
 from nose.tools import assert_true
+from pprint import pprint as pp
 import requests
 
-ROOT = "http://localhost:4284"
+API = "http://localhost:4284/api/"
 
 
 def test_server_available():
-    pass
+    response = requests.get(API + "hello")
+    assert_true(response.ok)
