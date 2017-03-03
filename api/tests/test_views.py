42667abe4093bbba7907cf4f1522df0370da7291
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-03-03 11:17:30 +0100

    Add fn to convert bytes to dict

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -11,20 +11,23 @@ import requests
 API = "http://localhost:4284/api/"
 
 
+def json_to_dict(data):
+    if isinstance(data, bytes):
+         data = data.decode('utf-8')
+    return json.loads(data)
+
+
 def get_response(route):
     return requests.get(API + route)
 
 
-def parse_status(response):
-    data = json.loads(response.content).get("status")
-    if isinstance(data, bytes):
-        data = data.decode('utf-8')
-    return data
+def parse_status(content):
+    return json_to_dict(content).get("status")
 
 
 def test_server_available():
     response = get_response("hello")
-    status = parse_status(response)
+    status = parse_status(response.content)
     assert_true(response.ok)
     assert_equals("ok", status)
 
@@ -33,6 +36,6 @@ def test_login_error():
     credentials = json.dumps({"nickname": "foo",
                               "password": "bar"})
     response = requests.post(API + "login", data=credentials)
-    status = parse_status(response)
+    status = parse_status(response.content)
     assert_false(response.ok)
     assert_equals("error", status)
