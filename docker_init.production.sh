e68a49a6414433bb8091536b320429aad48d6da7
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-03-08 14:32:20 +0100

    Docker changes

--- docker_init.production.sh
+++ docker_init.production.sh
@@ -13,6 +13,12 @@ while true; do
     printf "\n# Deploying D-BAS...\n"
     python setup.py --quiet develop
 
+    printf "\n# Seeding discussion database...\n"
+    init_discussion_sql docker.ini #> /dev/null 2>&1
+
+    printf "\n# Seeding news database...\n"
+    init_news_sql docker.ini #> /dev/null 2>&1
+
     printf "\n# Compiling JS files...\n"
     google-closure-compiler-js --createSourceMap --compilationLevel SIMPLE ./dbas/static/js/{main,ajax,discussion,review}/*.js > dbas/static/js/dbas.min.js
 
@@ -20,18 +26,6 @@ while true; do
     sass dbas/static/css/main.sass dbas/static/css/main.css --style compressed
     rm -r .sass-cache
 
-    #printf "\n# Seeding discussion database...\n"
-    #init_discussion_sql docker.ini > /dev/null 2>&1
-
-    printf "\n# Drop old database...\n"
-    psql -U postgres bash -c "dropdb -U postgres discussion --if-exists" > /dev/null 2>&1
-    psql -U postgres bash -c "dropdb -U postgres news --if-exists" > /dev/null 2>&1
-
-    #printf "\n# Restoring previous discussion database...\n"
-    psql -U postgres bash -c "psql discussion < db/recent_db.sql" > /dev/null 2>&1
-
-    printf "\n# Seeding news database...\n"
-    init_news_sql docker.ini > /dev/null 2>&1
 
     printf "\n# Starting integrated web server -- for development use only!\n"
     uwsgi --ini-paste docker.ini
