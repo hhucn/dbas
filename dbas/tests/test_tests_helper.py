83e3ed779a9a83647e265086dd5b4bb47656aef8
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-03-15 10:50:58 +0100

    Fix test of helper test

--- dbas/tests/test_tests_helper.py
+++ dbas/tests/test_tests_helper.py
@@ -9,8 +9,8 @@ from dbas.helper.tests import add_settings_to_appconfig
 
 def test_settings_to_appconfig():
     config_default = str(add_settings_to_appconfig())
-    config_docker = str(add_settings_to_appconfig("docker.ini"))
-    assert "docker.ini" not in config_default
+    config_docker = str(add_settings_to_appconfig("production.ini"))
+    assert "production.ini" not in config_default
     assert "development.ini" in config_default
-    assert "docker.ini" in config_docker
+    assert "production.ini" in config_docker
     assert "development.ini" not in config_docker
