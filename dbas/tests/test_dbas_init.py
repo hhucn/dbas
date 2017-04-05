bd88583c2d9d87fc385dc9a4291a456baa3c9867
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-04-05 19:49:48 +0200

    Fix environs to key test in respect to the lowering

--- dbas/tests/test_dbas_init.py
+++ dbas/tests/test_dbas_init.py
@@ -3,9 +3,9 @@ from nose.tools import assert_equal
 
 
 def test_parse_env_name():
-    assert_equal("TEST", _environs_to_keys("DBAS_TEST", prefix="DBAS_"))
-    assert_equal("TEST", _environs_to_keys("TEST", prefix=""))
-    assert_equal("FOO.BAR", _environs_to_keys("DBAS_FOO_BAR", prefix="DBAS_"))
-    assert_equal("FOO.BAR.BAZ", _environs_to_keys("DBAS_FOO_BAR_BAZ", prefix="DBAS_"))
-    assert_equal("FOO_BAR", _environs_to_keys("DBAS_FOO__BAR", prefix="DBAS_"))
-    assert_equal("FOO__BAR", _environs_to_keys("DBAS_FOO___BAR", prefix="DBAS_"))
+    assert_equal("test", _environs_to_keys("DBAS_TEST", prefix="DBAS_"))
+    assert_equal("test", _environs_to_keys("TEST", prefix=""))
+    assert_equal("foo.bar", _environs_to_keys("DBAS_FOO_BAR", prefix="DBAS_"))
+    assert_equal("foo.bar.baz", _environs_to_keys("DBAS_FOO_BAR_BAZ", prefix="DBAS_"))
+    assert_equal("foo_bar", _environs_to_keys("DBAS_FOO__BAR", prefix="DBAS_"))
+    assert_equal("foo__bar", _environs_to_keys("DBAS_FOO___BAR", prefix="DBAS_"))
