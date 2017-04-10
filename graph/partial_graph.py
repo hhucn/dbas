62348ca84b89cd02a3c88c68e8a6857f4bc69440
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-04-10 13:04:03 +0200

    Filter disabled arguments in graph view

--- graph/partial_graph.py
+++ graph/partial_graph.py
@@ -191,10 +191,11 @@ def __append_todos_for_getting_argument_net_with_conclusion(uid, db_argument, li
     :return:
     """
     if db_argument.conclusion_uid is not None:
-        db_concl_args = DBDiscussionSession.query(Argument).filter_by(conclusion_uid=db_argument.conclusion_uid).all()
+        db_concl_args = DBDiscussionSession.query(Argument).filter(Argument.conclusion_uid == db_argument.conclusion_uid,
+                                                                   Argument.is_disabled == False).all()
 
         # get new todos
-        logger('PartialGraph', '__get_argument_net', 'conclusi ({}) args: {}'.format(db_argument.conclusion_uid, [arg.uid for arg in db_concl_args]))
+        logger('PartialGraph', '__get_argument_net', 'conclusion ({}) args: {}'.format(db_argument.conclusion_uid, [arg.uid for arg in db_concl_args]))
         for arg in db_concl_args:
             if arg.uid not in list_todos + list_dones + [uid]:
                 list_todos.append(arg.uid)
