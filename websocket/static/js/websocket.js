46c43a3804e543905225632a2c62d1fd824af3cb
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-04-10 18:20:31 +0200

    Replace http with ws in websocket.js

--- websocket/static/js/websocket.js
+++ websocket/static/js/websocket.js
@@ -34,9 +34,9 @@ function doConnect(){
 	
 	// switch between a local (http) and a global (https) mode
 	var dict = {query: 'nickname=' + $('#' + headerNicknameId).text(), secure: true};
-	var address =  'http://localhost';
+	var address =  'ws://localhost';
 	if (mainpage.indexOf('localhost') === -1) {
-		address = location.origin;
+		address = location.origin.replace('https', 'ws').replace('http', 'ws');
 		dict.secure = true;
 	}
 
