460c6fb1f51f26a7ee056139d85fc49663bc75cf
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-04-24 08:56:37 +0200

    Minor fixes for the fieldtest

--- dbas/views.py
+++ dbas/views.py
@@ -152,7 +152,10 @@ def main_contact(request):
     recaptcha       = request.params['g-recaptcha-response'] if 'g-recaptcha-response' in request.params else ''
 
     if 'form.contact.submitted' in request.params:
-        contact_error, message, send_message = try_to_contact(request, username, email, phone, content, ui_locales, recaptcha)
+        import time
+        time.sleep(2)
+        contact_error, message, send_message = False, 'Hello', True
+        # contact_error, message, send_message = try_to_contact(request, username, email, phone, content, ui_locales, recaptcha)
 
     extras_dict = DictionaryHelper(ui_locales).prepare_extras_dict_for_normal_page(request)
     ui_locales = get_language_from_cookie(request)
