1ab3b8ae49c640d6fcb34218e014a4ccf737fb1f
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-05-05 09:50:51 +0200

    New tour from discussion page (Fix #404)

--- dbas/static/js/main/guided-tour.js
+++ dbas/static/js/main/guided-tour.js
@@ -7,6 +7,8 @@ function GuidedTour(){
     
 	var lang_switcher = '';
 	var tour = '';
+	var step_list = [];
+	var start_from_index = false;
 		
 	// override default template for i18n
 	var template =
@@ -50,21 +52,22 @@ function GuidedTour(){
 	var start_fct = function () {
 		tour.init(); // Initialize the tour
 		tour.restart(); // Start the tour
-		setLocalStorage(GUIDED_TOUR_RUNNING, true);
+		setLocalStorage(GUIDED_TOUR_RUNNING_START, start_from_index);
+		setLocalStorage(GUIDED_TOUR_RUNNING_DISCUSSION, !start_from_index);
 		set_lang_click();
 	};
 	
 	// function on end
 	var end_fct = function(){
-		setLocalStorage(GUIDED_TOUR_RUNNING, false);
+		setLocalStorage(GUIDED_TOUR_RUNNING_START, false);
+		setLocalStorage(GUIDED_TOUR_RUNNING_DISCUSSION, false);
 		Cookies.set(GUIDED_TOUR, true, { expires: 60 });
-		// var url = window.location.href;
-		//if (url !== mainpage && url.indexOf('#tour2') === -1) {
-		//	 window.location.href = mainpage;
-		//}
 	};
 	
-	this.prepareSteps = function(){
+	/**
+	 * Prepares the steps of the tour as well as the tour itself
+	 */
+	this.prepareSteps = function(startFromIndex){
 		// lang switcher
 		if (getLanguage() === 'en'){
 			var de_flag = $('#' + translationLinkDe).find('img').attr('src');
@@ -82,7 +85,7 @@ function GuidedTour(){
 			placement: 'bottom',
 		};
 		var start_button = {
-			element: '#start-discussion-button',
+			element: '#start-text',
 			title: _t(tourStartButtonTitle) + lang_switcher,
 			content: _t(tourStartButtonContent),
 			placement: 'bottom',
@@ -98,20 +101,19 @@ function GuidedTour(){
 			title: _t(tourIssueTitle) + lang_switcher,
 			content: _t(tourIssueContent),
 			placement: 'bottom',
+			path: '/discuss',
 		};
 		var start_discussion = {
 			element: '#dialog-speech-bubbles-space',
 			title: _t(tourStartDiscussionTitle) + lang_switcher,
 			content: _t(tourStartDiscussionContent),
 			placement: 'bottom',
-			path: '/discuss'
 		};
 		var mark_opinion = {
 			element: '#some-element-bubble .triangle-r',
 			title: _t(tourMarkOpinionTitle) + lang_switcher,
 			content: _t(tourMarkOpinionContent),
 			placement: 'bottom',
-			path: '/discuss',
 			onShow: function () {
 				var element = '<div class="line-wrapper-r" id="some-element-bubble">' +
 					'<p class="triangle-r"><i class="fa fa-star-o" aria-hidden="true" style="padding-right: 0.5em"></i>' +
@@ -122,26 +124,17 @@ function GuidedTour(){
 				$('#some-element-bubble').remove();
 			},
 		};
-		var sidebar = {
-			element: '.sidebar-wrapper',
-			title: _t(tourStartDiscussionTitle) + lang_switcher,
-			content: _t(tourStartDiscussionContent),
-			placement: 'bottom',
-			path: '/discuss'
-		};
 		var choose_answer = {
 			element: '#discussions-space-list',
 			title: _t(tourSelectAnswertTitle) + lang_switcher,
 			content: _t(tourSelectAnswertContent),
 			placement: 'bottom',
-			path: '/discuss'
 		};
 		var set_input = {
 			element: '#discussions-space-list li:last-child',
 			title: _t(tourEnterStatementTitle) + lang_switcher,
 			content: _t(tourEnterStatementContent),
 			placement: 'bottom',
-			path: '/discuss'
 		};
 		var first_child = $('#discussions-space-list li:first');
 		var statement_action = {
@@ -149,7 +142,6 @@ function GuidedTour(){
 			title: _t(tourStatementActionTitle) + lang_switcher,
 			content: _t(tourStatementActionContent),
 			placement: 'bottom',
-			path: '/discuss',
 			onShow: function () {
 				first_child.trigger("mouseenter");
 			},
@@ -158,29 +150,39 @@ function GuidedTour(){
 			}
 		};
 		var have_fun = {
-			element: '.jumbotron',
+			element: startFromIndex? '.jumbotron' : '#discussion-container',
 			title: _t(tourHaveFunTitle) + lang_switcher,
 			content: _t(tourHaveFunContent),
 			placement: 'bottom',
-			path: '/#tour2',
+			path: startFromIndex ? '/#tour2' : '/discuss',
 			template: template_end
 		};
 		
-		//data-placement="bottom"
-		tour = new Tour({
-			steps: [
-				welcome,            // 0
+		if (startFromIndex){
+			step_list = [
 				start_button,       // 1
 				//login_button,
 				issue,              // 2
 				start_discussion,   // 3
 				mark_opinion,       // 4
-				sidebar,            // 5
-				choose_answer,      // 6
-				set_input,          // 7
-				statement_action,   // 8
-				have_fun,           // 9
-				],
+				choose_answer,      // 5
+				set_input,          // 6
+				statement_action,   // 7
+				have_fun,           // 8
+				];
+		} else {
+			step_list = [
+				issue,              // 0
+				start_discussion,   // 1
+				mark_opinion,       // 2
+				choose_answer,      // 3
+				set_input,          // 4
+				have_fun,           // 5
+			];
+		}
+		//data-placement="bottom"
+		tour = new Tour({
+			steps: step_list,
 			backdrop: true,
 			backdropPadding: 5,
 			template: template,
@@ -194,7 +196,12 @@ function GuidedTour(){
 	 */
 	this.start = function(){
 		//displayBubbleInformationDialog();
-		this.prepareSteps();
+		this.prepareSteps(true);
+		
+		// are we on the discuss-page?
+		var split = window.location.href.replace('discuss#', 'discuss').split('/');
+		var pos = split.indexOf('discuss');
+		var startOnDiscussionPage = split.length === pos + 2 || split.length === pos + 1;
 		
 		// params
 		var url = window.location.href;
@@ -203,48 +210,73 @@ function GuidedTour(){
 		var part2 = url === mainpage + '#tour2' || url === location.origin + '#tour2';
 		
 		// decision where we are
-		if (part0 && !part1 && !part2){ // first part on the start page, where we ask the user for a tour
-			// build dialog
-			var title = _t(tourWelcomeTitle);
-			var text = _t(welcomeDialogBody) + lang_switcher.replace('style="', 'style="display: none; ');
-			var dialog = $('#' + popupConfirmDialogId);
-			dialog.on('shown.bs.modal', function (){
-				var switcher = getLanguage() === 'en' ? $('#switch-to-de') : $('#switch-to-en');
-				switcher.detach();
-				switcher.removeClass('pull-right').addClass('pull-left');
-				switcher.insertBefore($('#' + popupConfirmDialogAcceptBtn)).show();
-			});
-			dialog.on('hide.bs.modal', function (){
-				var switcher = getLanguage() === 'en' ? $('#switch-to-de') : $('#switch-to-en');
-				switcher.remove();
-			});
-			displayConfirmationDialog(title, text, start_fct, end_fct, false);
-			dialog.find('#confirm-dialog-accept-btn').text(_t(yes));
-			dialog.find('#confirm-dialog-refuse-btn').text(_t(no));
-			set_lang_click();
-		} else if(!part0 && part1 && !part2 && getLocalStorage(GUIDED_TOUR_RUNNING) === 'true'){ // part 2: discussion
-			tour.init();
-			tour.goTo(2);
-			set_lang_click();
-			setLocalStorage(GUIDED_TOUR_RUNNING, true);
-		} else if(!part0 && !part1 && part2 && getLocalStorage(GUIDED_TOUR_RUNNING) === 'true'){ // part 3, the end
-			tour.init();
-			tour.goTo(9);
-			set_lang_click();
-			setLocalStorage(GUIDED_TOUR_RUNNING, true);
+		if (part0 && !part1 && !part2){
+			start_from_index = true;
+			this.__startTour();
+		} else if(!part0 && part1 && !part2 || startOnDiscussionPage){
+			if (getLocalStorage(GUIDED_TOUR_RUNNING_START) === 'true') {
+				console.log(0);
+				this.__continueIndexTourOnDiscussionPage();
+			} else if (startOnDiscussionPage){
+				start_from_index = false;
+				this.prepareSteps(false);
+				console.log(1);
+				this.__startTour();
+			}
 			
-			var btn1 = $('.btn-warning');
-			var btn2 = $('.btn-info');
-			var url1 = btn1.attr('href');
-			var url2 = btn2.attr('href');
-			btn1.click(function(){
-				end_fct();
-				window.location.href = url1;
-			}).attr('href', '#');
-			btn2.click(function(){
-				end_fct();
-				window.location.href = url2;
-			}).attr('href', '#');
+		} else if(!part0 && !part1 && part2 && getLocalStorage(GUIDED_TOUR_RUNNING_START) === 'true' && getLocalStorage(GUIDED_TOUR_RUNNING_DISCUSSION) === 'false'){
+			this.__endIndexTourOnDiscussionPage();
 		}
+	};
+	
+	this.__startTour = function(){
+		// build dialog
+		var title = _t(tourWelcomeTitle);
+		var text = _t(welcomeDialogBody) + lang_switcher.replace('style="', 'style="display: none; ');
+		var dialog = $('#' + popupConfirmDialogId);
+		
+		// add language switcher
+		dialog.on('shown.bs.modal', function (){
+			var switcher = getLanguage() === 'en' ? $('#switch-to-de') : $('#switch-to-en');
+			switcher.detach();
+			switcher.removeClass('pull-right').addClass('pull-left');
+			switcher.insertBefore($('#' + popupConfirmDialogAcceptBtn)).show();
+		});
+		dialog.on('hide.bs.modal', function (){
+			var switcher = getLanguage() === 'en' ? $('#switch-to-de') : $('#switch-to-en');
+			switcher.remove();
+		});
+		displayConfirmationDialog(title, text, start_fct, end_fct, false);
+		dialog.find('#confirm-dialog-accept-btn').text(_t(yes));
+		dialog.find('#confirm-dialog-refuse-btn').text(_t(no));
+		set_lang_click();
+	};
+	
+	this.__continueIndexTourOnDiscussionPage = function(){
+		tour.init();
+		tour.goTo(1);
+		set_lang_click();
+		setLocalStorage(GUIDED_TOUR_RUNNING_START, true);
+	};
+	
+	this.__endIndexTourOnDiscussionPage = function(){
+		tour.init();
+		tour.goTo(step_list.length - 1);
+		set_lang_click();
+		setLocalStorage(GUIDED_TOUR_RUNNING_START, true);
+		
+		// modify both buttons on start page for ending the tour
+		var btn1 = $('.btn-warning');
+		var btn2 = $('.btn-info');
+		var url1 = btn1.attr('href');
+		var url2 = btn2.attr('href');
+		btn1.click(function(){
+			end_fct();
+			window.location.href = url1;
+		}).attr('href', '#');
+		btn2.click(function(){
+			end_fct();
+			window.location.href = url2;
+		}).attr('href', '#');
 	};
 }
