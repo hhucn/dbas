a02e8b8abee14f163a34c9b0e0968b2b949bbafb
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-05-09 20:43:39 +0200

    Fix error with tests on user page

--- tests/test_frontend/test_PagesWhenNotLoggedIn.py
+++ tests/test_frontend/test_PagesWhenNotLoggedIn.py
@@ -56,5 +56,6 @@ def test_admin_pages():
 
 
 def test_user_pages():
-    browser.visit('{}/user/{}'.format(ROOT, 2))
+    browser.visit('{}/user/{}'.format(ROOT, 3))
     assert_true(browser.is_text_present('Tobias'), error_text)
+    assert_true(browser.is_text_present('Reputation'), error_text)
