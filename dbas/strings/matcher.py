49e4aba3976d7a5c89ea937a4c04c71fa8a3eb9d
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-08-04 14:18:33 +0200

    Fix test

--- dbas/strings/matcher.py
+++ dbas/strings/matcher.py
@@ -60,7 +60,7 @@ def get_prediction(request, _tn, for_api, api_data, request_authenticated_userid
     elif mode == '3' or mode == '4':  # adding reasons / duplicates
         try:
             uid = int(extra)
-        except TypeError and ValueError:
+        except TypeError or ValueError:
             uid = None
 
         return_dict['values'] = get_strings_for_duplicates_or_reasons(value, issue, uid)
