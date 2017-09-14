0a12eb73939f7e2d0f108b7283a07c64eab37c02
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-09-14 20:26:18 +0200

    Remove pyramid config dependency

--- db-migrations/env.py
+++ db-migrations/env.py
@@ -5,33 +5,13 @@ be loaded from there.
 
 """
 from alembic import context
-from paste.deploy import loadapp
-from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from dbas import get_dbas_environs
 from pyramid.paster import get_appsettings
 from dbas.database import get_db_environs, DiscussionBase
-import logging
-
-# disable logging from dbas/__init__.py
-logging.disable(logging.DEBUG)
-
-try:
-    # if pylons app already in, don't create a new app
-    from pylons import config as pylons_config
-    pylons_config['__file__']
-except:
-    config = context.config
-    # can use config['__file__'] here, i.e. the Pylons
-    # ini file, instead of alembic.ini
-    config_file = config.get_main_option('pylons_config_file')
-    fileConfig(config_file)
-    wsgi_app = loadapp('config:%s' % config_file, relative_to='.')
-
 
 # customize this section for non-standard engine configurations.
 meta = __import__('dbas.database').database
-# meta = __import__("%s.model.meta" % wsgi_app.config['pylons.package']).model.meta
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -67,12 +47,6 @@ def run_migrations_online():
 
     """
     # specify here how the engine is acquired
-    def process_revision_directives(context, revision, directives):
-        if config.cmd_opts.autogenerate:
-            script = directives[0]
-            if script.upgrade_ops.is_empty():
-                directives[:] = []
-
     engine = _get_dbas_engine()
 
     with engine.connect() as connection:
