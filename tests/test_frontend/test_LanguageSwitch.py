57ab1bd019006023fa6e15de9c47047df28aedc2
feger <marc.feger@hhu.de>
2017-09-21 14:25:59 +0200

    Add correct tag for codeauthor

--- tests/test_frontend/test_LanguageSwitch.py
+++ tests/test_frontend/test_LanguageSwitch.py
@@ -1,28 +1,25 @@
 """
-author: Marc Feger <marc.feger@uni-duesseldorf.de>
+This tests need a short timeout at the beginning to make sure everything has loaded correctly.
+Please visit __init__.py for more details about the parameters.
 
+There may be some problems with the pipeline if it runs the tests.
+This may have the cases.
+    1: PhantomJs has SSL certificate errors -> add service_args
+        Also visit: http://phantomjs.org/api/command-line.html for more details
+    2: Pipeline is not ready with all its previous jobs -> add more TIME_TO_PREPARE
 
-meta:
-    This tests need a short timeout at the beginning to make sure everything has loaded correctly.
-    Please visit __init__.py for more details about the parameters.
+The tests if the language had changed by checking the source page and the cookies.
+It is important to simulate a correct browser behavior -> visit Path -> refresh -> show data.
+If the browser changes the correctly the changed language should be visible after every refresh regarding to
+the previous changes( english -> german a.s.o)
 
-    There may be some problems with the pipeline if it runs the tests.
-    This may have the cases.
-        1: PhantomJs has SSL certificate errors -> add service_args
-            Also visit: http://phantomjs.org/api/command-line.html for more details
-        2: Pipeline is not ready with all its previous jobs -> add more TIME_TO_PREPARE
+It will be tests in the cookies and the page_source:
+    english -> english
+    english -> german
+    german  -> german
+    german  -> english
 
-    The tests if the language had changed by checking the source page and the cookies.
-    It is important to simulate a correct browser behavior -> visit Path -> refresh -> show data.
-    If the browser changes the correctly the changed language should be visible after every refresh regarding to
-    the previous changes( english -> german a.s.o)
-
-    It will be tests in the cookies and the page_source:
-        english -> english
-        english -> german
-        german  -> german
-        german  -> english
-
+.. codeauthor:: Marc Feger <marc.feger@uni-duesseldorf.de>
 """
 
 from . import *
@@ -204,7 +201,7 @@ def test_german_to_english_cookies():
     service_args:  to prevent ssl v3 error
     cookies[len(cookies) - 1].get('value'): because the value of the language is always a dictionary
                                             at the last place of cookies.
-                                            
+
     :return: Test in the cookies if the language changed from german to english
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
