e162ada335914da8dbf5969b504c5cc324ffd767
feger <marc.feger@hhu.de>
2017-09-21 10:50:10 +0200

    Add DocStrings and constant time to prepare the loadings

--- tests/test_frontend/__init__.py
+++ tests/test_frontend/__init__.py
@@ -4,7 +4,7 @@ import logging
 from selenium.webdriver.remote.remote_connection import LOGGER
 
 LOGGER.setLevel(logging.WARNING)
-_multiprocess_can_split_ = False # this could be a problem, because
+_multiprocess_can_split_ = False  # this could be a problem, because
 
 ROOT = 'http://localhost:4284'
 PATH = '/ajax_switch_language?_LOCALE_='
@@ -14,3 +14,5 @@ LANGUAGE = {'GERMAN': 'de', 'ENGLISH': 'en'}
 
 TEST_STRING = {'GERMAN': 'Teil des Graduierten-Kollegs', 'ENGLISH': 'part of the graduate'}
 TEST_ID = {'GERMAN': 'id="switch-lang-indicator-de"', 'ENGLISH': 'id="switch-lang-indicator-en"'}
+
+TIME_TO_PREPARE = 5
--- tests/test_frontend/test_LanguageSwitch.py
+++ tests/test_frontend/test_LanguageSwitch.py
@@ -1,14 +1,13 @@
 from . import *
+from selenium import webdriver
 import time
 
 
 def setup():
-    time.sleep(5)
-    # wait a long time to see if the first test runs
+    time.sleep(TIME_TO_PREPARE)  # very ugly but important to prepare the tests
 
 
 def teardown():
-    from selenium import webdriver
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
     driver.get(ROOT + PATH + LANGUAGE["ENGLISH"])
     driver.get(ROOT)
@@ -16,8 +15,11 @@ def teardown():
     driver.close()
 
 
-def test_english_to_english():
-    from selenium import webdriver
+def test_english_to_english_page_source():
+    """
+    service_args:  to prevent ssl v3 error
+    :return: Test in the page_source if the national flag changed from english to english
+    """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
     driver.get(ROOT + PATH + LANGUAGE["ENGLISH"])
     driver.get(ROOT)
@@ -27,13 +29,15 @@ def test_english_to_english():
         html_content = driver.page_source
         assert_in(TEST_ID["ENGLISH"], html_content)
         assert_not_in(TEST_ID["GERMAN"], html_content)
-
     finally:
         driver.close()
 
 
-def test_english_to_german():
-    from selenium import webdriver
+def test_english_to_german_page_source():
+    """
+    service_args:  to prevent ssl v3 error
+    :return: Test in the page_source if the national flag changed from english to german
+    """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
     driver.get(ROOT + PATH + LANGUAGE["GERMAN"])
     driver.get(ROOT)
@@ -43,13 +47,15 @@ def test_english_to_german():
         html_content = driver.page_source
         assert_in(TEST_ID["GERMAN"], html_content)
         assert_not_in(TEST_ID["ENGLISH"], html_content)
-
     finally:
         driver.close()
 
 
-def test_german_to_german():
-    from selenium import webdriver
+def test_german_to_german_page_source():
+    """
+    service_args:  to prevent ssl v3 error
+    :return: Test in the page_source if the national flag changed from german to german
+    """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
     driver.get(ROOT + PATH + LANGUAGE["GERMAN"])
     driver.get(ROOT)
@@ -59,13 +65,15 @@ def test_german_to_german():
         html_content = driver.page_source
         assert_in(TEST_ID["GERMAN"], html_content)
         assert_not_in(TEST_ID["ENGLISH"], html_content)
-
     finally:
         driver.close()
 
 
-def test_german_to_english():
-    from selenium import webdriver
+def test_german_to_english_page_source():
+    """
+    service_args:  to prevent ssl v3 error
+    :return: Test in the page_source if the national flag changed from german to english
+    """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
     driver.get(ROOT + PATH + LANGUAGE["ENGLISH"])
     driver.get(ROOT)
