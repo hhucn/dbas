c0ab08b4b46612c9458634db3e18b31e627620de
feger <marc.feger@hhu.de>
2017-09-21 14:14:22 +0200

    Add missing lines in Documentations

--- tests/test_frontend/test_LanguageSwitch.py
+++ tests/test_frontend/test_LanguageSwitch.py
@@ -1,7 +1,3 @@
-from . import *
-from selenium import webdriver
-import time
-
 """
 author: Marc Feger <marc.feger@uni-duesseldorf.de>
 
@@ -9,18 +5,18 @@ author: Marc Feger <marc.feger@uni-duesseldorf.de>
 meta:
     This tests need a short timeout at the beginning to make sure everything has loaded correctly.
     Please visit __init__.py for more details about the parameters.
-    
+
     There may be some problems with the pipeline if it runs the tests.
     This may have the cases.
-        1: PhantomJs has SSL certificate errors -> add service_args 
+        1: PhantomJs has SSL certificate errors -> add service_args
             Also visit: http://phantomjs.org/api/command-line.html for more details
         2: Pipeline is not ready with all its previous jobs -> add more TIME_TO_PREPARE
-        
+
     The tests if the language had changed by checking the source page and the cookies.
     It is important to simulate a correct browser behavior -> visit Path -> refresh -> show data.
-    If the browser changes the correctly the changed language should be visible after every refresh regarding to 
+    If the browser changes the correctly the changed language should be visible after every refresh regarding to
     the previous changes( english -> german a.s.o)
-    
+
     It will be tests in the cookies and the page_source:
         english -> english
         english -> german
@@ -28,6 +24,10 @@ meta:
         german  -> english
 
 """
+
+from . import *
+from selenium import webdriver
+import time
 
 
 def setup():
@@ -45,6 +45,7 @@ def teardown():
 def test_english_to_english_page_source():
     """
     service_args:  to prevent ssl v3 error
+
     :return: Test in the page_source if the national flag changed from english to english
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
@@ -63,6 +64,7 @@ def test_english_to_english_page_source():
 def test_english_to_german_page_source():
     """
     service_args:  to prevent ssl v3 error
+
     :return: Test in the page_source if the national flag changed from english to german
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
@@ -81,6 +83,7 @@ def test_english_to_german_page_source():
 def test_german_to_german_page_source():
     """
     service_args:  to prevent ssl v3 error
+
     :return: Test in the page_source if the national flag changed from german to german
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
@@ -99,6 +102,7 @@ def test_german_to_german_page_source():
 def test_german_to_english_page_source():
     """
     service_args:  to prevent ssl v3 error
+
     :return: Test in the page_source if the national flag changed from german to english
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
@@ -119,6 +123,7 @@ def test_english_to_english_cookies():
     service_args:  to prevent ssl v3 error
     cookies[len(cookies) - 1].get('value'): because the value of the language is always a dictionary
                                             at the last place of cookies.
+
     :return: Test in the cookies if the language changed from english to english
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
@@ -145,6 +150,7 @@ def test_english_to_german_cookies():
     service_args:  to prevent ssl v3 error
     cookies[len(cookies) - 1].get('value'): because the value of the language is always a dictionary
                                             at the last place of cookies.
+
     :return: Test in the cookies if the language changed from english to german
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
@@ -171,6 +177,7 @@ def test_german_to_german_cookies():
     service_args:  to prevent ssl v3 error
     cookies[len(cookies) - 1].get('value'): because the value of the language is always a dictionary
                                             at the last place of cookies.
+
     :return: Test in the cookies if the language changed from german to german
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
@@ -197,6 +204,7 @@ def test_german_to_english_cookies():
     service_args:  to prevent ssl v3 error
     cookies[len(cookies) - 1].get('value'): because the value of the language is always a dictionary
                                             at the last place of cookies.
+                                            
     :return: Test in the cookies if the language changed from german to english
     """
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
