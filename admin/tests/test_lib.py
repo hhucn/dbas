66236b866eff8d6fe5edc759657926dd1ab7439b
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-10-16 15:20:40 +0200

    Add generate/validate round trip test for API-Tokens

--- admin/tests/test_lib.py
+++ admin/tests/test_lib.py
@@ -5,7 +5,7 @@ Unit tests for lib.py
 """
 import unittest
 
-from admin.lib import get_overview, get_table_dict, update_row, delete_row, add_row, table_mapper
+import admin.lib as admin
 from dbas.database import DBDiscussionSession, get_dbas_db_configuration
 from dbas.database.discussion_model import User
 from dbas.helper.tests import add_settings_to_appconfig
@@ -31,16 +31,16 @@ new_user = {
 class AdminTest(unittest.TestCase):
 
     def test_get_overview(self):
-        dict_return = get_overview('some_main_page')
+        dict_return = admin.get_overview('some_main_page')
         for row in dict_return:
             for category in row:
                 for table in category['content']:
-                    self.assertTrue(table['name'].lower() in table_mapper)
-                    self.assertTrue('some_main_page' + table['name'] == table['href'])
+                    self.assertIn(table['name'].lower(), admin.table_mapper)
+                    self.assertEqual('some_main_page' + table['name'], table['href'])
 
     def test_get_table_dict(self):
-        for table in table_mapper:
-            return_dict = get_table_dict(table_mapper[table]['name'], 'some_main_page')
+        for table in admin.table_mapper:
+            return_dict = admin.get_table_dict(admin.table_mapper[table]['name'], 'some_main_page')
             self.assertIn('is_existing', return_dict)
             self.assertIn('name', return_dict)
             self.assertNotIn('password', return_dict)
@@ -50,13 +50,13 @@ class AdminTest(unittest.TestCase):
     def test_add_row(self):
         translator = Translator('en')
 
-        return_val = add_row('User', new_user, 'Tobia', translator)
+        return_val = admin.add_row('User', new_user, 'Tobia', translator)
         self.assertEqual(return_val, translator.get(_.noRights))
 
-        return_val = add_row('Userr', new_user, 'Tobias', translator)
+        return_val = admin.add_row('Userr', new_user, 'Tobias', translator)
         self.assertEqual(return_val, translator.get(_.internalKeyError))
 
-        return_val = add_row('User', new_user, 'Tobias', translator)
+        return_val = admin.add_row('User', new_user, 'Tobias', translator)
         self.assertEqual(len(return_val), 0)
 
         db_new_user = DBDiscussionSession.query(User).filter_by(nickname=new_user['nickname']).all()
@@ -68,38 +68,44 @@ class AdminTest(unittest.TestCase):
         keys = ['nickname']
         values = ['TheRenamedOne']
 
-        return_val = update_row('User', uids, keys, values, 'Tobia', translator)
-        self.assertTrue(return_val == translator.get(_.noRights))
+        return_val = admin.update_row('User', uids, keys, values, 'Tobia', translator)
+        self.assertEqual(return_val, translator.get(_.noRights))
 
-        return_val = update_row('Userr', uids, keys, values, 'Tobias', translator)
-        self.assertTrue(return_val == translator.get(_.internalKeyError))
+        return_val = admin.update_row('Userr', uids, keys, values, 'Tobias', translator)
+        self.assertEqual(return_val, translator.get(_.internalKeyError))
 
         db_old_user = DBDiscussionSession.query(User).order_by(User.uid.asc()).first()
-        return_val = update_row('User', [db_old_user.uid], keys, values, 'Tobias', translator)
+        return_val = admin.update_row('User', [db_old_user.uid], keys, values, 'Tobias', translator)
         db_old_user = DBDiscussionSession.query(User).filter_by(nickname=new_user['nickname']).all()
         db_new_user = DBDiscussionSession.query(User).filter_by(nickname=values[0]).all()
-        self.assertTrue(len(return_val) == 0)
-        self.assertTrue(len(db_old_user) == 0)
-        self.assertTrue(len(db_new_user) == 1)
+        self.assertEqual(len(return_val), 0)
+        self.assertEqual(len(db_old_user), 0)
+        self.assertEqual(len(db_new_user), 1)
 
-        db_reset = update_row('User', [db_new_user[0].uid], keys, [nick_of_anonymous_user], 'Tobias', translator)
-        self.assertTrue(len(db_reset) == 0)
+        db_reset = admin.update_row('User', [db_new_user[0].uid], keys, [nick_of_anonymous_user], 'Tobias', translator)
+        self.assertEqual(len(db_reset), 0)
 
     def test_delete_row(self):
         translator = Translator('en')
 
-        return_val = delete_row('User', [0], 'Tobia', translator)
-        self.assertTrue(return_val == translator.get(_.noRights))
+        return_val = admin.delete_row('User', [0], 'Tobia', translator)
+        self.assertEqual(return_val, translator.get(_.noRights))
 
-        return_val = delete_row('Userr', [0], 'Tobias', translator)
-        self.assertTrue(return_val == translator.get(_.internalKeyError))
+        return_val = admin.delete_row('Userr', [0], 'Tobias', translator)
+        self.assertEqual(return_val, translator.get(_.internalKeyError))
 
         db_new_user = DBDiscussionSession.query(User).filter(User.firstname == new_user['firstname'],
                                                              User.surname == new_user['surname']).all()
-        self.assertTrue(len(db_new_user) == 1)
+        self.assertEqual(len(db_new_user), 1)
 
-        return_val = delete_row('user', [db_new_user[0].uid], 'Tobias', translator)
-        self.assertTrue(len(return_val) == 0)
+        return_val = admin.delete_row('user', [db_new_user[0].uid], 'Tobias', translator)
+        self.assertEqual(len(return_val), 0)
         db_new_user = DBDiscussionSession.query(User).filter(User.firstname == new_user['firstname'],
                                                              User.surname == new_user['surname']).all()
-        self.assertTrue(len(db_new_user) == 0)
+        self.assertEqual(len(db_new_user), 0)
+
+
+class APITokenTest(unittest.TestCase):
+    def test_generate_check(self):
+        token = admin.generate_application_token("test")
+        self.assertTrue(admin.check_token(token))
