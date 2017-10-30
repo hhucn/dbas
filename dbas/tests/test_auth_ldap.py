3e3d506c870d5234e3cc030602c404fa8f1dc6c6
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-10-30 20:54:05 +0100

    Fix test because settings moved from ini to env

--- dbas/tests/test_auth_ldap.py
+++ dbas/tests/test_auth_ldap.py
@@ -16,19 +16,9 @@ class AuthLdapTest(unittest.TestCase):
         testing.tearDown()
 
     def test_verify_ldap_user_data(self):
-        registry_settings = {
-            'settings:ldap:server': 'ldaps://ldaps.ad.hhu.de',
-            'settings:ldap:base': 'ou=IDMUsers,DC=AD,DC=hhu,DC=de',
-            'settings:ldap:account.scope': '@ad.hhu.de',
-            'settings:ldap:account.filter': 'sAMAccountName',
-            'settings:ldap:account.firstname': 'givenName',
-            'settings:ldap:account.lastname': 'sn',
-            'settings:ldap:account.title': 'personalTitle',
-            'settings:ldap:account.email': 'mail'
-        }
         nickname = 'Bob'
         password = 'iamatestuser2016'
         _tn = Translator('en')
-        response = verify_ldap_user_data(registry_settings, nickname, password, _tn)
+        response = verify_ldap_user_data(nickname, password, _tn)
         self.assertTrue(response['error'] in [_tn.get(_.serviceNotAvailable) + '. ' + _tn.get(_.pleaseTryAgainLaterOrContactUs),
                                               _tn.get(_.userPasswordNotMatch)])
