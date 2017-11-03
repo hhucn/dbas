643a6a970a3a5da0d068edc42b86d69774230958
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-11-03 17:10:10 +0100

    Add documentation about Manual migrations
    Remove old roles from database if they exist.

--- docker/db/migrations/v1.5.1/501_transfer_owner_to_postgres.sh
+++ docker/db/migrations/v1.5.1/501_transfer_owner_to_postgres.sh
@@ -3,3 +3,8 @@ for tbl in `psql -qAt -U postgres -c "select tablename from pg_tables where sche
 
 # ALTER news schema
 for tbl in `psql -qAt -U postgres -c "select tablename from pg_tables where schemaname = 'news';" discussion` ; do  psql -U postgres -c "alter table news.\"$tbl\" owner to postgres" discussion ; done
+
+dropuser -U postgres --if-exists dbas
+dropuser -U postgres --if-exists writer
+dropuser -U postgres --if-exists dolan
+dropuser -U postgres --if-exists read_only_discussion
