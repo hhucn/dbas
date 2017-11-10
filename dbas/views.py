ad44b9df068f86d98e3a8542c218103865a93608
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2017-11-10 15:07:58 +0100

    Cleaner urls

--- dbas/views.py
+++ dbas/views.py
@@ -1050,16 +1050,13 @@ def user_login_oauth(request):
     try:
         service = request.params['service']
         url = request.params['redirect_uri']
+        # cleaner urls
         if '?service' in url:
             url = url[0:url.index('/discuss') + len('/discuss')] + url[url.index('?service'):]
-        logger('X', 'X', str([issue.slug for issue in DBDiscussionSession.query(Issue).all()]))
         for slug in [issue.slug for issue in DBDiscussionSession.query(Issue).all()]:
             if slug in url:
                 url = url[0:url.index('/discuss') + len('/discuss')]
-        logger('X', 'X', url)
-        logger('X', 'X', url)
-        redirect_url = url
-        # redirect_url = '{}/discuss?service={}'.format(request.application_url, service).replace('http:', 'https:')
+        redirect_url = url.replace('http:', 'https:')
 
         val = login_user_oauth(request, service, redirect_url, lang)
         if val is None:
