579f243f05e5b7ae68c3f829fc56915b57914e51
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-11-21 14:49:20 +0100

    Add check for api_token to api

--- api/login.py
+++ api/login.py
@@ -6,12 +6,13 @@ Logic for user login, token generation and validation
 
 import binascii
 import hashlib
-import json
 import os
+
+import json
+import transaction
 from datetime import datetime
 
-import transaction
-
+from admin.lib import check_token
 from dbas.auth.login import login_user
 from dbas.database import DBDiscussionSession
 from dbas.database.discussion_model import User
@@ -53,7 +54,7 @@ def _get_user_by_nickname(nickname):
 def process_user(request, payload):
     htoken = payload["token"]
     try:
-        user, token = htoken.split('-', 1)
+        user, token = htoken.split('-', 1)  # The 1 is important, because api tokens themself have their own dash.
     except ValueError:
         log.info("[API] Could not split htoken: {}".format(htoken))
         raise HTTP401()
@@ -62,7 +63,7 @@ def process_user(request, payload):
 
     db_user = _get_user_by_nickname(user)
 
-    if not db_user.token == token:
+    if not db_user.token == token and not check_token(token):
         log.info("[API] Invalid Token")
         raise HTTP401()
 
