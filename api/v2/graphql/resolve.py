f9a573061b9a5b06a14dff3d6bca7c97dc181273
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2018-01-04 16:49:54 +0100

    Just query not disabled rows by default

--- api/v2/graphql/resolve.py
+++ api/v2/graphql/resolve.py
@@ -3,9 +3,10 @@ Namespace for functions used to resolve Queries on the database for GraphQL.
 
 .. sectionauthor:: Christian Meter <meter@cs.uni-duesseldorf.de>
 """
+from typing import Dict
 
 
-def resolve_list_query(args, context, graph):
+def resolve_list_query(args: Dict, context, graph):
     """
     Query a list of objects based on the provided graph / model.
     Removes disabled items and adds all additional arguments as a filter for sqlalchemy.
@@ -17,9 +18,13 @@ def resolve_list_query(args, context, graph):
     :rtype: list
     """
     query = graph.get_query(context)
-    if args:
-        query = query.filter_by(**args)
-    return query.all()
+
+    # just apply the filter to the ones who actually have a 'is_disabled' column
+    if any([column == 'is_disabled' for column in dir(query.column_descriptions[0]['entity'])]):
+        disabled = args['is_disabled'] if 'is_disabled' in args else False  # this enables querying for disabled rows
+        query = query.filter_by(is_disabled=disabled)
+
+    return query.filter_by(**args).all()
 
 
 def resolve_field_query(args, context, graph):
