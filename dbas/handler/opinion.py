76627168fc966f943be07957fc7cb505c9306212
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2018-01-30 10:01:53 +0100

    Fix check for not given argument

--- dbas/handler/opinion.py
+++ dbas/handler/opinion.py
@@ -301,9 +301,13 @@ def get_user_with_same_opinion_for_argument(argument_uid, nickname, lang, main_p
     :return: {'users':[{nickname1.avatar_url, nickname1.vote_timestamp}*]}
     """
     try:
-        logger('OpinionHandler', 'get_user_with_same_opinion_for_argument', 'Argument {}: {}'.format(argument_uid, get_text_for_argument_uid(argument_uid, 'de')))
+        text = get_text_for_argument_uid(argument_uid, 'de')
+        logger('OpinionHandler', 'get_user_with_same_opinion_for_argument', 'Argument {}: {}'.format(argument_uid, text))
+        if not text:
+            return None
     except TypeError:
         return None
+
     db_user = DBDiscussionSession.query(User).filter_by(nickname=nickname).first()
     db_user_uid = db_user.uid if db_user else 0
 
