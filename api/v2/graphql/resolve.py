f2788084435a12cc7e7c28fd722a170cde28eff6
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2018-02-28 00:22:07 +0100

    Cherry pick 8c633ada

--- api/v2/graphql/resolve.py
+++ api/v2/graphql/resolve.py
@@ -18,11 +18,7 @@ def resolve_list_query(args: Dict, context, graph):
     :rtype: list
     """
     query = graph.get_query(context)
-
-    # just apply the filter to the ones who actually have a 'is_disabled' column
-    if any([column == 'is_disabled' for column in dir(query.column_descriptions[0]['entity'])]):
-        disabled = args['is_disabled'] if 'is_disabled' in args else False  # this enables querying for disabled rows
-        query = query.filter_by(is_disabled=disabled)
+    query = __default(query, args, 'is_disabled', False)
 
     return query.filter_by(**args).all()
 
@@ -42,4 +38,23 @@ def resolve_field_query(args, context, graph):
         query = query.get(args.get("uid"))
     else:
         query = query.filter_by(**args).first()
+    return query
+
+
+def __default(query, args, key, default_value):
+    """
+    Filters a query by a value provided in args or the default given.
+    Does nothing if `key` is not in args.
+
+    :param query: The query to filter
+    :param args: Arguments for querying. These are not applied yet!
+    :param key: A key to look for in the query model
+    :param default_value: The value 'key' gets if there is no value in args
+    :return:
+    """
+    # just apply the filter to the ones who actually have a 'is_disabled' column
+    if key in dir(query.column_descriptions[0]['entity']):
+        disabled = args[key] if key in args else default_value  # this enables querying for disabled rows
+        query = query.filter_by(**{key: disabled})
+
     return query
