ddd5a38c39017df06537e133f2cf9e8422f354f7
feger <marc.feger@hhu.de>
2018-03-07 16:02:59 +0100

    Refactor UlrManager arguments to fit newer version; add requester_tests;

--- search/requester.py
+++ search/requester.py
@@ -2,7 +2,7 @@ import requests
 
 from dbas.database import DBDiscussionSession
 from dbas.database.discussion_model import Issue
-from dbas.url_manager import UrlManager
+from dbas.helper.url import UrlManager
 from search.routes import get_statements_with_value_path, get_duplicates_or_reasons_path, \
     get_edits_path, get_suggestions_path
 
@@ -18,8 +18,8 @@ def get_suggestions(issue_uid: int, is_start_point: bool, search=""):
 
 def get_statements_with_value(issue_uid: int, application_url, search=""):
     query = get_statements_with_value_path(issue_uid, search)
-    slug = DBDiscussionSession.query(Issue).get("4").slug
-    _um = UrlManager(application_url, for_api=False, slug=slug)
+    slug = DBDiscussionSession.query(Issue).get(issue_uid).slug
+    _um = UrlManager(slug=slug)
 
     results = []
     current_results = request_as_json(query)["result"]
@@ -29,7 +29,7 @@ def get_statements_with_value(issue_uid: int, application_url, search=""):
             "statement_uid": res["statement_uid"],
             "content": res["content"],
             "score": res["score"],
-            "url": _um.get_url_for_statement_attitude(False, res["statement_uid"])
+            "url": _um.get_url_for_statement_attitude(res["statement_uid"])
         }, current_results))
 
     return results
