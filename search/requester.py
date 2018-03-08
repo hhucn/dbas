946ddbbfba9b21b3b542f2bee1b782218e62cd76
feger <marc.feger@hhu.de>
2018-03-08 15:50:29 +0100

    Add doc-strings and type annotation; rename is_startpoint to position in search;

--- search/requester.py
+++ search/requester.py
@@ -8,16 +8,40 @@ from search.routes import get_statements_with_value_path, get_duplicates_or_reas
 
 
 def request_as_json(query: str) -> dict:
+    """
+    Request with a certain query and return the result as a dict.
+
+    :param query: path to search at
+    :return: return results as a dict
+    """
     return requests.get(query).json()
 
 
-def get_suggestions(issue_uid: int, is_start_point: bool, search: str = "") -> dict:
-    query = get_suggestions_path(issue_uid, is_start_point, search)
+def get_suggestions(issue_uid: int, position: bool, search_value: str = "") -> dict:
+    """
+    Return the search results for suggestions of the textversions of statements fitting
+    the parametes.
+
+    :param issue_uid: uid of the issue to search in
+    :param position: the position of the statement
+    :param search_value: the text to be searched for
+    :return: suggestions of the textversions fitting the given parameters
+    """
+    query = get_suggestions_path(issue_uid, position, search_value)
     return request_as_json(query)["result"]
 
 
-def get_statements_with_value(issue_uid: int, search: str = "") -> dict:
-    query = get_statements_with_value_path(issue_uid, search)
+def get_statements_with_value(issue_uid: int, search_value: str = "") -> list:
+    """
+    This method returns statements fitting the given parametes.
+    It returns the result as a list of single dicts containing the information of each result
+    with the data: text, statement_uid, content, score, url.
+
+    :param issue_uid: uid of the issue to search in
+    :param search_value: the position of the statement
+    :return: statements fitting a certain text
+    """
+    query = get_statements_with_value_path(issue_uid, search_value)
     slug = DBDiscussionSession.query(Issue).get(issue_uid).slug
     _um = UrlManager(slug=slug)
 
@@ -35,11 +59,27 @@ def get_statements_with_value(issue_uid: int, search: str = "") -> dict:
     return results
 
 
-def get_duplicates_or_reasons(issue_uid: int, statement_uid: int, search: str = "") -> dict:
-    query = get_duplicates_or_reasons_path(issue_uid, statement_uid, search)
+def get_duplicates_or_reasons(issue_uid: int, statement_uid: int, search_value: str = "") -> dict:
+    """
+    This method returns suggestions for duplicated or reasoned statements.
+
+    :param issue_uid: uid of the issue to search in
+    :param statement_uid: uid of the statement which is supposed to be a duplicate or reason
+    :param search_value: text to be searched for
+    :return: duplicates or reasons fitting the parameters
+    """
+    query = get_duplicates_or_reasons_path(issue_uid, statement_uid, search_value)
     return request_as_json(query)["result"]
 
 
-def get_edits(issue_uid: int, statement_uid: int, search=""):
-    query = get_edits_path(issue_uid, statement_uid, search)
+def get_edits(issue_uid: int, statement_uid: int, search_value=""):
+    """
+    This method returns suggestions for edits fitting a the parameters.
+
+    :param issue_uid: uid of the issue to search in
+    :param statement_uid: uid of the statement with edits
+    :param search_value: text to be searched for
+    :return: edits fitting the parameters
+    """
+    query = get_edits_path(issue_uid, statement_uid, search_value)
     return request_as_json(query)["result"]
--- search/routes.py
+++ search/routes.py
@@ -1,21 +1,56 @@
 from search import ROUTE_API
 
 
-def get_statements_with_value_path(issue_uid: int, search: str = "") -> str:
-    suffix = "/statements?id={}&search={}".format(issue_uid, search)
+def get_statements_with_value_path(issue_uid: int, search_value: str = "") -> str:
+    """
+    Create the query string to get statements matching a certain string.
+    This method contains all parameters used in search(service).
+
+    :param issue_uid: uid of the issue to search in
+    :param search_value: text to be searched for
+    :return: query satisfied the requirements of search(service) to get statements fitting search_value
+    """
+    suffix = "/statements?id={}&search={}".format(issue_uid, search_value)
     return ROUTE_API + suffix
 
 
-def get_duplicates_or_reasons_path(issue_uid: int, statement_uid: int, search: str = "") -> str:
-    suffix = "/duplicates_reasons?id={}&statement_uid={}&search={}".format(issue_uid, statement_uid, search)
+def get_duplicates_or_reasons_path(issue_uid: int, statement_uid: int, search_value: str = "") -> str:
+    """
+    Create the query string to get duplicates or reasons matching a certain string.
+    This method contains all parameters used in search(service).
+
+    :param issue_uid: uid of the issue to search in
+    :param search_value: text to be searched for
+    :return: query satisfied the requirements of search(service)
+    :return: query satisfied the requirements of search(service) to get duplicates or reasons fitting search_value
+    """
+    suffix = "/duplicates_reasons?id={}&statement_uid={}&search={}".format(issue_uid, statement_uid, search_value)
     return ROUTE_API + suffix
 
 
-def get_edits_path(issue_uid: int, statement_uid: int, search: str = "") -> str:
-    suffix = "/edits?id={}&statement_uid={}&search={}".format(issue_uid, statement_uid, search)
+def get_edits_path(issue_uid: int, statement_uid: int, search_value: str = "") -> str:
+    """
+    Create the query string to get edits matching a certain string to a given statement uid.
+    This method contains all parameters used in search(service).
+
+    :param issue_uid: uid of the issue to search in
+    :param statement_uid: uid of the statement which is supposed to be a duplicate or reason
+    :param search_value: text to be searched for
+    :return: query satisfied the requirements of search(service) to get edits fitting search_value
+    """
+    suffix = "/edits?id={}&statement_uid={}&search={}".format(issue_uid, statement_uid, search_value)
     return ROUTE_API + suffix
 
 
-def get_suggestions_path(issue_uid: int, is_startpoint: bool, search: str = "") -> str:
-    suffix = "/suggestions?id={}&start={}&search={}".format(issue_uid, is_startpoint, search)
+def get_suggestions_path(issue_uid: int, position: bool, search_value: str = "") -> str:
+    """
+    Create the query string to get suggestions matching a certain string of a given issue.
+    This method contains all parameters used in search(service).
+
+    :param issue_uid: uid of the issue to search in
+    :param position: position of the statement
+    :param search_value: text to be searched for
+    :return: query satisfied the requirements of search(service) to get suggestions fitting search_value
+    """
+    suffix = "/suggestions?id={}&start={}&search={}".format(issue_uid, position, search_value)
     return ROUTE_API + suffix
