d19ac072876356c4fe74321e906fdf60a1fd5bcb
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-03-09 16:19:03 +0100

    Remove unreachable code in valid_position

--- dbas/validators/discussion.py
+++ dbas/validators/discussion.py
@@ -151,10 +151,6 @@ def valid_position(request):
         if db_position.issue_uid != request.validated['issue'].uid:
             add_error(request, 'Position does not belong to the queried issue', location='path')
             return False
-        if db_position.is_disabled:
-            add_error(request, 'Position is disabled', location='path', status_code=410)
-            return False
-
         request.validated['position']: Statement = db_position
         return True
     return False
