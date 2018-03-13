5a7edd9720a39608fe51d3f8539b10f62d3fba3f
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-03-13 10:06:47 +0100

    Unittestify database validator tests

--- dbas/validators/tests/test_database.py
+++ dbas/validators/tests/test_database.py
@@ -1,74 +1,48 @@
-from cornice import Errors
-from nose.tools import assert_true, assert_equal, assert_false
-from pyramid import testing
-
 from dbas.database.discussion_model import Statement
+from dbas.tests.utils import TestCaseWithConfig, construct_dummy_request
 from dbas.validators.database import valid_table_name, valid_database_model
 
 
-def test_valid_table_name():
-    request = testing.DummyRequest(json_body={})
-    setattr(request, 'errors', Errors())
-    setattr(request, 'cookies', {'_LOCALE_': 'en'})
-    response = valid_table_name(request)
-    assert_false(response)
-    assert_equal(bool, type(response))
-
-    request = testing.DummyRequest(json_body={'table': 'Stateme'})
-    setattr(request, 'errors', Errors())
-    setattr(request, 'cookies', {'_LOCALE_': 'en'})
-    response = valid_table_name(request)
-    assert_false(response)
-    assert_equal(bool, type(response))
-
-    for t in ['statement', 'StAtement']:
-        request = testing.DummyRequest(json_body={'table': t})
-        setattr(request, 'errors', Errors())
-        request.validated = {}
+class TestValidDatabase(TestCaseWithConfig):
+    def test_valid_table_name(self):
+        request = construct_dummy_request()
         response = valid_table_name(request)
-        assert_true(response)
-        assert_equal(bool, type(response))
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
-    setattr(request, 'errors', Errors())
-    setattr(request, 'cookies', {'_LOCALE_': 'en'})
-    response = valid_table_name(request)
-    assert_true(response)
-    assert_equal(bool, type(response))
+        request = construct_dummy_request({'table': 'Stateme'})
+        response = valid_table_name(request)
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
+        for t in ['statement', 'StAtement']:
+            request = construct_dummy_request({'table': t})
+            response = valid_table_name(request)
+            self.assertTrue(response)
+            self.assertIsInstance(response, bool)
 
-def test_valid_database_model():
-    request = testing.DummyRequest(json_body={})
-    setattr(request, 'errors', Errors())
-    fn = valid_database_model('', '')
-    response = fn(request)
-    assert_false(response)
-    assert_equal(bool, type(response))
+    def test_valid_database_model(self):
+        request = construct_dummy_request()
+        response = valid_database_model('', '')(request)
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
-    request = testing.DummyRequest(json_body={})
-    setattr(request, 'errors', Errors())
-    fn = valid_database_model('k', '')
-    response = fn(request)
-    assert_false(response)
-    assert_equal(bool, type(response))
+        request = construct_dummy_request()
+        response = valid_database_model('k', '')(request)
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
-    request = testing.DummyRequest(json_body={})
-    setattr(request, 'errors', Errors())
-    fn = valid_database_model('', 't')
-    response = fn(request)
-    assert_false(response)
-    assert_equal(bool, type(response))
+        request = construct_dummy_request()
+        response = valid_database_model('', 't')(request)
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
-    request = testing.DummyRequest(json_body={'k': 0})
-    setattr(request, 'errors', Errors())
-    fn = valid_database_model('k', Statement)
-    response = fn(request)
-    assert_false(response)
-    assert_equal(bool, type(response))
+        request = construct_dummy_request({'k': 0})
+        response = valid_database_model('k', Statement)(request)
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
-    request = testing.DummyRequest(json_body={'k': 1})
-    request.validated = {}
-    setattr(request, 'errors', Errors())
-    fn = valid_database_model('k', Statement)
-    response = fn(request)
-    assert_true(response)
-    assert_equal(bool, type(response))
+        request = construct_dummy_request({'k': 1})
+        response = valid_database_model('k', Statement)(request)
+        self.assertTrue(response)
+        self.assertIsInstance(response, bool)
