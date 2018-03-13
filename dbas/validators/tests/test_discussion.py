2ed86ff439381964d5f73f04c2324faa528c157e
feger <marc.feger@hhu.de>
2018-03-13 16:36:18 +0100

    rewrite test for fuzzy search validator;

--- dbas/validators/tests/test_discussion.py
+++ dbas/validators/tests/test_discussion.py
@@ -378,23 +378,23 @@ class TestValidStatementOrArgId(TestCaseWithConfig):
         self.assertIn('stmt_or_arg', request.validated)
 
     def test_valid_fuzzy_search_mode(self):
-        request = self.__prepare_dict({})
+        request = construct_dummy_request({'type': None})
         response = discussion.valid_fuzzy_search_mode(request)
-        self.assert_false(response)
-        self.assert_equal(bool, type(response))
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
-        request = self.__prepare_dict({'type': ''})
+        request = construct_dummy_request({'type': ''})
         response = discussion.valid_fuzzy_search_mode(request)
-        self.assert_false(response)
-        self.assert_equal(bool, type(response))
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
-        request = self.__prepare_dict({'type': -1})
+        request = construct_dummy_request({'type': -1})
         response = discussion.valid_fuzzy_search_mode(request)
-        self.assert_false(response)
-        self.assert_equal(bool, type(response))
+        self.assertFalse(response)
+        self.assertIsInstance(response, bool)
 
         for mode in [0, 1, 2, 3, 4, 5, 8, 9]:
-            request = self.__prepare_dict({'type': mode})
+            request = construct_dummy_request({'type': mode})
             response = discussion.valid_fuzzy_search_mode(request)
-            self.assert_true(response)
-            self.assert_equal(bool, type(response))
+            self.assertTrue(response)
+            self.assertIsInstance(response, bool)
