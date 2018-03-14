4329e58e65f1332d8f80fa6fc3a97775d16cb80f
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-03-14 14:55:46 +0100

    Fix tests of test_user and unittestify them

--- dbas/validators/tests/test_user.py
+++ dbas/validators/tests/test_user.py
@@ -10,32 +10,30 @@ class Usertest(TestCaseWithConfig):
         request = construct_dummy_request()
         response = user.valid_user(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         self.config.testing_securitypolicy(userid='hello', permissive=True)
         response = user.valid_user(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         self.config.testing_securitypolicy(userid='Tobias', permissive=True)
         response = user.valid_user(request)
         self.assertTrue(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
     def test_valid_user_as_author_of_statement(self):
         request = construct_dummy_request()
         response = user.valid_user_as_author_of_statement(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         for id in ['', 'hello', 'anonymous']:
             self.config.testing_securitypolicy(userid=id, permissive=True)
             for el in ['', 'a', '0', '1', 1]:
-                request = testing.DummyRequest(json_body={'uid': el})
-                request.validated = {}
-                setattr(request, 'errors', Errors())
+                request = construct_dummy_request({'statement_id': el})
                 response = user.valid_user_as_author_of_statement(request)
-                self.assertEqual(bool, type(response))
+                self.assertIsInstance(response, bool)
                 if id == 'anonymous' and el in ['1', 1]:
                     self.assertTrue(response)
                 else:
@@ -53,7 +51,7 @@ class Usertest(TestCaseWithConfig):
                 request.validated = {}
                 setattr(request, 'errors', Errors())
                 response = user.valid_user_as_author_of_argument(request)
-                self.assertEqual(bool, type(response))
+                self.assertIsInstance(response, bool)
                 if id == 'anonymous' and el in ['1', 1]:
                     self.assertTrue(response)
                 else:
@@ -63,41 +61,41 @@ class Usertest(TestCaseWithConfig):
         request = construct_dummy_request()
         response = user.valid_user_as_author(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         self.config.testing_securitypolicy(userid='Pascal', permissive=True)
         response = user.valid_user_as_author(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         self.config.testing_securitypolicy(userid='Tobias', permissive=True)
         response = user.valid_user_as_author(request)
         self.assertTrue(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
     def test_valid_user_as_admin(self):
         request = construct_dummy_request()
         response = user.valid_user_as_admin(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         self.config.testing_securitypolicy(userid='Pascal', permissive=True)
         response = user.valid_user_as_admin(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         self.config.testing_securitypolicy(userid='Tobias', permissive=True)
         response = user.valid_user_as_admin(request)
         self.assertTrue(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
     def test_invalid_user(self):
         request = construct_dummy_request()
         response = user.invalid_user(request)
         self.assertFalse(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
 
         self.config.testing_securitypolicy(userid='Tobias', permissive=True)
         response = user.invalid_user(request)
         self.assertTrue(response)
-        self.assertEqual(bool, type(response))
+        self.assertIsInstance(response, bool)
