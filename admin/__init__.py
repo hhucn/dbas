9eae83a85beb4d762d8cbaebfb97d5ce6aaecba8
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2018-03-29 11:16:14 +0200

    Update init

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,25 +1,8 @@
 """
-REST API for the admin interface of D-BAS.
+Views for the admin interface
 
 .. sectionauthor:: Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
 """
-from pyramid.config import Configurator
-from dbas.logger import logger
-
-
-def init(config):
-    config.scan("admin.views")
-
-
-def main(global_config, **settings):
-    config = Configurator(settings=settings)
-    init(config)
-    return config.make_wsgi_app()
-
 
 def includeme(config):
-    init(config)
-
-
-if __name__ == "__main__":
-    logger("i", "in too")
+    config.scan("admin.views")
