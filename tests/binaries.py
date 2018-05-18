43ad0d67dea6a6481993e1c3839e8d3f1c3cce42
Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de>
2018-05-18 10:44:58 +0200

    Add script to test binaries #560

---
+++ tests/binaries.py
@@ -0,0 +1,25 @@
+import subprocess
+
+existent_binaries = [
+    ('uwsgi', '--version'),
+    ('alembic', '--help'),
+    ('pserve', '--help'),
+]
+
+functional_binaries = [
+    ('alembic', 'current'),
+    ('pserve', 'development.ini'),
+]
+
+# check for binaries
+for (binary, argument) in existent_binaries:
+    print('Run: {} {}'.format(binary, argument))
+    result = subprocess.run([binary, argument], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    assert 'not found' not in result
+
+# test binaries
+for (binary, argument) in functional_binaries:
+    print('Test: {} {}'.format(binary, argument))
+    result = subprocess.run([binary, argument], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    assert 'not found' not in result
+
