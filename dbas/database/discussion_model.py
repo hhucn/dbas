bba3bbb6df9950801d83714177f05a8f839af0a9
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-07-30 18:03:40 +0200

    Add hybrid proerties to statement references and an abstraction for the API

--- dbas/database/discussion_model.py
+++ dbas/database/discussion_model.py
@@ -3,9 +3,8 @@ D-BAS database Model
 
 .. codeauthor:: Tobias Krauthoff <krauthoff@cs.uni-duesseldorf.de
 """
-import warnings
-
 import arrow
+import warnings
 from cryptacular.bcrypt import BCRYPTPasswordManager
 from slugify import slugify
 from sqlalchemy import Integer, Text, Boolean, Column, ForeignKey, DateTime, String
@@ -570,9 +569,9 @@ class StatementReferences(DiscussionBase):
     users = relationship('User', foreign_keys=[author_uid])
     issues = relationship('Issue', foreign_keys=[issue_uid])
 
-    def __init__(self, reference, host, path, author_uid, statement_uid, issue_uid):
+    def __init__(self, reference: str, host: str, path: str, author_uid: int, statement_uid: int, issue_uid: int):
         """
-        Inits a row in current statements reference table
+        Store a real-world text-reference.
 
         :param reference: String
         :param host: Host of URL
@@ -588,6 +587,14 @@ class StatementReferences(DiscussionBase):
         self.author_uid = author_uid
         self.statement_uid = statement_uid
         self.issue_uid = issue_uid
+
+    @hybrid_property
+    def issue(self) -> Issue:
+        return DBDiscussionSession.query(Issue).get(self.issue_uid)
+
+    @hybrid_property
+    def statement(self) -> Statement:
+        return DBDiscussionSession.query(Statement).get(self.statement_uid)
 
     def get_statement_text(self, html: bool = False) -> str:
         """
