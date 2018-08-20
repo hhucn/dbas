391528e6494f75a3502abd3997e8ef4aae7455eb
feger <marc.feger@uni-duesseldorf.de>
2018-08-20 14:07:34 +0200

    Remove uncommented code

--- frontendtest/cypress/support/index.js
+++ frontendtest/cypress/support/index.js
@@ -15,12 +15,3 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-/**
- Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-});**/
