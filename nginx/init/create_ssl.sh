12b11bd3d4556cd9960a7a9110fdae220783ef8b
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-08-29 13:34:01 +0200

    Set CN Name for SSL certificate in docker-compose file

--- nginx/init/create_ssl.sh
+++ nginx/init/create_ssl.sh
@@ -2,4 +2,4 @@
 
 [ -d /etc/nginx/ssl ] || mkdir -p /etc/nginx/ssl
 apk --update add openssl
-openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=localhost"
+openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=${DEPLOY_HOST}"
