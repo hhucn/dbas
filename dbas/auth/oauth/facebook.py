4bdf9ad070d909cbd8c10af543455d5df521fba0
Alexander Schneider <alexander@schneider.gg>
2018-09-06 15:12:26 +0200

    Refactor logging in auth.oauth.facebook

--- dbas/auth/oauth/facebook.py
+++ dbas/auth/oauth/facebook.py
@@ -7,14 +7,12 @@ Used lib: http://requests-oauthlib.readthedocs.io/en/latest/examples/facebook.ht
 Manage Google Client IDs: https://developers.facebook.com/apps/
 """
 
+import logging
 import os
-
 from pyramid.httpexceptions import HTTPBadRequest
 from pyramid.request import Request
 from requests_oauthlib.compliance_fixes import facebook_compliance_fix
 from requests_oauthlib.oauth2_session import OAuth2Session
-
-from dbas.logger import logger
 
 CLIENT_ID = os.environ.get('OAUTH_FACEBOOK_CLIENTID', None)
 CLIENT_SECRET = os.environ.get('OAUTH_FACEBOOK_CLIENTKEY', None)
@@ -28,11 +26,13 @@ def start_flow(request: Request, redirect_uri: str = None) -> dict:
     """
     Starts the oauth flow. This will return a dict which causes a redirect to the providers page.
 
+    :param request: The Pyramid Request associated with the OAuth request.
     :param redirect_uri: The URL to redirect to.
     :return:
     """
     next_url = request.application_url + '/discuss' if not redirect_uri else redirect_uri
-    logger('Facebook OAuth', 'Read OAuth id/secret: none? {}/{}'.format(CLIENT_ID is None, CLIENT_SECRET is None))
+    log = logging.getLogger(__name__)
+    log.debug("Read OAuth id/secret: none? %s/%s", CLIENT_ID is None, CLIENT_SECRET is None)
     oauth_session = OAuth2Session(CLIENT_ID, scope=','.join(SCOPE),
                                   redirect_uri=(request.application_url + REDIRECT_PATH))
     oauth_session = facebook_compliance_fix(oauth_session)
@@ -42,7 +42,7 @@ def start_flow(request: Request, redirect_uri: str = None) -> dict:
     request.session['csrf'] = state
     request.session['next'] = next_url
 
-    logger('Facebook OAuth', 'Please go to {} and authorize access'.format(authorization_url))
+    log.debug("Please go to %s and authorize access", authorization_url)
     return {'authorization_url': authorization_url, 'error': ''}
 
 
@@ -51,7 +51,8 @@ def continue_flow(request: Request):
         raise HTTPBadRequest('CSRF Error')
 
     oauth_session = request.session['oauth_session']
-    logger('Facebook OAuth', 'Read OAuth id/secret: none? {}/{}'.format(CLIENT_ID is None, CLIENT_SECRET is None))
+    log = logging.getLogger(__name__)
+    log.debug("Read OAuth id/secret: none? %s/%s", CLIENT_ID is None, CLIENT_SECRET is None)
 
     oauth_session.fetch_token(TOKEN_URL,
                               client_secret=CLIENT_SECRET,
