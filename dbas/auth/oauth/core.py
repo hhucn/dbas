0354a945ca8f22d823e62b46a0b99409885fc192
Alexander Schneider <alexander@schneider.gg>
2018-09-06 15:08:33 +0200

    Refactor logging in auth.oauth.core

--- dbas/auth/oauth/core.py
+++ dbas/auth/oauth/core.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+import logging
 from pyramid.httpexceptions import HTTPFound
 from pyramid.request import Request
 from pyramid.view import view_config
@@ -10,7 +11,6 @@ from dbas.database import DBDiscussionSession
 from dbas.database.discussion_model import Group
 from dbas.handler import user
 from dbas.handler.language import get_language_from_cookie
-from dbas.logger import logger
 from dbas.strings.keywords import Keywords as _
 from dbas.strings.translator import Translator
 
@@ -28,12 +28,13 @@ def oauth_start(request: Request) -> dict:
 def oauth_endpoint(request: Request):
     service = request.matchdict.get("service")
     ui_local = get_language_from_cookie(request)
-    logger("oauth endpoint", service)
+    log = logging.getLogger(__name__)
+    log.debug("%s", service)
 
     data, next_url = PROVIDER[service].continue_flow(request)
-    user = set_oauth_user(data, service, ui_local)["user"]
+    oauth_user = set_oauth_user(data, service, ui_local)["user"]
 
-    headers, url = __refresh_headers_and_url(request, user, False, next_url)
+    headers, url = __refresh_headers_and_url(request, oauth_user, False, next_url)
     sleep(0.5)
     return HTTPFound(location=url, headers=headers)
 
@@ -49,8 +50,9 @@ def set_oauth_user(user_data, service, ui_locales):
     _tn = Translator(ui_locales)
 
     db_group = DBDiscussionSession.query(Group).filter_by(name='users').first()
+    log = logging.getLogger(__name__)
     if not db_group:
-        logger('Auth.Login', 'Error occured')
+        log.debug("Error occured: No db_group for users during `set_oauth_user`")
         return {'error': _tn.get(_.errorTryLateOrContant)}
 
     ret_dict = user.set_new_oauth_user(user_data, user_data['id'], service, _tn)
