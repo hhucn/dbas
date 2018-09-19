9a9da692aa19e0550b9000bb62241a099f5f4151
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-09-19 13:21:11 +0200

    Bump version: 1.10.3 → 1.10.4

--- dbas/views/helper.py
+++ dbas/views/helper.py
@@ -13,7 +13,7 @@ from dbas.lib import usage_of_modern_bubbles, nick_of_anonymous_user, Attitudes
 
 LOG = logging.getLogger(__name__)
 name = 'D-BAS'
-version = '1.10.3'
+version = '1.10.4'
 full_version = version
 project_name = name + ' ' + full_version
 
