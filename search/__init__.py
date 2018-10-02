95b43a42005575fc6162d3dcab7c8abc8e5aed4b
feger <marc.feger@uni-duesseldorf.de>
2018-10-02 12:32:42 +0200

    Move env-vars for communication between search and dbas to __init__

--- search/__init__.py
+++ search/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-SEARCH_PROTOCOL = os.environ.get('DBAS_PROTOCOL')
-SEARCH_HOST = os.environ.get('SEARCH_NAME')
+SEARCH_PROTOCOL = os.environ.get('SEARCH_PROTOCOL', 'http')
+SEARCH_HOST = os.environ.get('SEARCH_NAME', 'search')
 SEARCH_PORT = os.environ.get('SEARCH_PORT', 5000)
 ROUTE_API = '{}://{}:{}'.format(SEARCH_PROTOCOL, SEARCH_HOST, SEARCH_PORT)
