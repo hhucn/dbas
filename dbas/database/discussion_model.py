668418f14c9cc0be5bbb379d8291c2944b6da5c3
TomatenMarc <marc.feger@uni-duesseldorf.de>
2018-11-05 09:34:10 +0100

    Redo wrong changes for development

--- dbas/database/discussion_model.py
+++ dbas/database/discussion_model.py
@@ -31,7 +31,10 @@ def sql_timestamp_pretty_print(ts, lang='en', humanize=True, with_exact_time=Fal
     :return: String
     """
     if humanize:
+        # if lang == 'de':
         ts = ts.to('Europe/Berlin')
+        # else:
+        #    ts = ts.to('US/Pacific')
         return ts.humanize(locale=lang)
     else:
         if lang == 'de':
@@ -89,7 +92,7 @@ class Issue(DiscussionBase):
         self.is_disabled = is_disabled
         self.is_private = is_private
         self.is_read_only = is_read_only
-        self.date = get_now().naive
+        self.date = get_now()
 
     @hybrid_property
     def lang(self):
