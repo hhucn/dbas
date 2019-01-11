587750d05d598785102e06334fbf00f03f6527be
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-01-11 14:26:12 +0100

    Readd tests for old auth method

--- api/tests/test_login.py
+++ api/tests/test_login.py
@@ -1,11 +1,11 @@
-import unittest
+import json
 
-from api.login import validate_credentials, valid_token
-from api.tests.test_views import create_request_with_token_header
-from dbas.tests.utils import construct_dummy_request
+from api.login import validate_credentials, valid_token, valid_token_optional
+from api.tests.test_views import create_request_with_token_header, user_tokens
+from dbas.tests.utils import construct_dummy_request, TestCaseWithConfig
 
 
-class ValidateCredentialsTest(unittest.TestCase):
+class ValidateCredentialsTest(TestCaseWithConfig):
     def test_valid_credentials(self):
         request = construct_dummy_request()
         request.validated = {
@@ -28,7 +28,7 @@ class ValidateCredentialsTest(unittest.TestCase):
         self.assertGreater(len(request.errors), 0)
 
 
-class ValidTokenTest(unittest.TestCase):
+class ValidTokenJWTTest(TestCaseWithConfig):
     header = 'Authorization'
 
     def test_invalid_token(self):
@@ -42,4 +42,36 @@ class ValidTokenTest(unittest.TestCase):
         request = create_request_with_token_header()
         valid_token(request)
         self.assertEqual(len(request.errors), 0)
+        self.assertIn('user', request.validated)
+
+
+class ValidTokenTest(TestCaseWithConfig):
+    header = 'X-Authentication'
+
+    def test_valid_token(self):
+        nickname = 'Walter'
+        token = user_tokens[nickname]
+        request = construct_dummy_request()
+        request.headers[self.header] = json.dumps(
+            {'nickname': nickname, 'token': token, 'additional key': 'will be ignored'})
+        valid_token(request)
+        self.assertEqual(request.errors, [])
+        self.assertIn('user', request.validated)
+
+    def test_invalid_nickname(self):
+        nickname = 'Walter_not_in_db'
+        token = 'mytoken'
+        request = construct_dummy_request()
+        request.headers[self.header] = json.dumps({'nickname': nickname, 'token': token})
+        valid_token(request)
+        self.assertGreater(len(request.errors), 0)
+        self.assertNotIn('user', request.validated)
+
+    def test_anonymous_user_ignore_optional(self):
+        nickname = 'anonymous'
+        token = 'mytoken'
+        request = construct_dummy_request()
+        request.headers[self.header] = json.dumps({'nickname': nickname, 'token': token})
+        valid_token_optional(request)
+        self.assertListEqual(request.errors, [])
         self.assertIn('user', request.validated)
