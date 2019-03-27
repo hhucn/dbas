28ccf7feca28cc8e89f4417e7b60f664365a3593
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2019-03-27 22:10:41 +0100

    Abort transaction, if cost couldn't be added

--- dbas/handler/statements.py
+++ dbas/handler/statements.py
@@ -46,6 +46,7 @@ def set_position(db_user: User, db_issue: Issue, statement_text: str, feature_da
 
     if db_issue.decision_process:
         if 'decidotron_cost' not in feature_data:
+            transaction.abort()
             return {
                 'status': 'fail',  # best error management
                 'error': 'Cost missing for an issue with a decision_process'
@@ -56,6 +57,7 @@ def set_position(db_user: User, db_issue: Issue, statement_text: str, feature_da
             if 0 < cost <= db_issue.decision_process.budget:
                 add_associated_cost(db_issue, new_statement, to_cents(cost))
             else:
+                transaction.abort()
                 return {
                     'status': 'fail',
                     'error': 'Cost has to be 0 < cost <= {}. (In cents)'.format(db_issue.decision_process.budget)
