hhucn/dbas

commit 5f65bfe0e7f4ba6e611c37e414a27e0f5cf5a92b

Allow a "free" proposal

dbas/views/discussion/json.py
@@ -188,7 +188,7 @@ def set_new_start_argument(request):
     prepared_dict_pos = set_position(request.validated['user'], request.validated['issue'],
                                      request.validated['position'], request.validated['feature_data'])
 
-    if len(prepared_dict_pos['error']) == 0:
+    if prepared_dict_pos['error']:
         LOG.debug("Set premise/reason")
         prepared_dict_pos = set_positions_premise(request.validated['issue'],
                                                   request.validated['user'],

api/views.py
@@ -652,6 +652,9 @@ def add_position_with_premise(request):
 
     new_position = set_position(db_user, db_issue, request.validated['position-text'])
 
+    if new_position['error']:
+        return new_position['error']
+
     conclusion_id: int = new_position['statement_uids'][0]
     db_conclusion: Statement = DBDiscussionSession.query(Statement).get(conclusion_id)
 

dbas/handler/statements.py
@@ -54,13 +54,13 @@ def set_position(db_user: User, db_issue: Issue, statement_text: str, feature_da
         else:
             cost = to_cents(float(feature_data['decidotron_cost']))
 
-            if 0 < cost <= db_issue.decision_process.budget:
+            if 0 <= cost <= db_issue.decision_process.budget:
                 add_associated_cost(db_issue, new_statement, to_cents(cost))
             else:
                 transaction.abort()
                 return {
                     'status': 'fail',
-                    'error': 'Cost has to be 0 < cost <= {}. (In cents)'.format(db_issue.decision_process.budget)
+                    'error': 'Cost has to be 0 <= cost <= {}. (In cents)'.format(db_issue.decision_process.budget)
                 }
 
     _um = UrlManager(db_issue.slug)

commit dcbc334d5b387e3f41af1a4ce1c59bf2209a9d15

Invert condition
bug introduced in 5f65bfe0e7f4ba6e611c37e414a27e0f5cf5a92b

dbas/views/discussion/json.py
@@ -188,7 +188,7 @@ def set_new_start_argument(request):
     prepared_dict_pos = set_position(request.validated['user'], request.validated['issue'],
                                      request.validated['position'], request.validated['feature_data'])
 
-    if prepared_dict_pos['error']:
+    if not prepared_dict_pos['error']:
         LOG.debug("Set premise/reason")
         prepared_dict_pos = set_positions_premise(request.validated['issue'],
                                                   request.validated['user'],