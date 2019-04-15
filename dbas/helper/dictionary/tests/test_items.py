0ca435d7ad9b990f7f5d3b78262fceb79ac91e76
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-04-15 20:30:46 +0200

    Use unittest-assertions instead of built-ins

--- dbas/helper/dictionary/tests/test_items.py
+++ dbas/helper/dictionary/tests/test_items.py
@@ -17,13 +17,11 @@ class TestShuffleList(unittest.TestCase):
         list0 = list(range(100))
         list1 = shuffle_list_by_user(self.db_user, list0)
         list2 = shuffle_list_by_user(self.db_user, list0)
-
         self.assertEqual(list1, list2)
 
     def test_not_equal(self):
         list0 = list(range(100))
         list1 = shuffle_list_by_user(self.db_user, list0)
-
         self.assertNotEqual(list0, list1)
 
 
@@ -31,11 +29,11 @@ class TestPrepareItemDictForAttitude(TestCaseWithConfig):
     def test_statement_with_children(self):
         idh = ItemDictHelper("de", self.issue_cat_or_dog)
         result_dict = idh.prepare_item_dict_for_attitude(4)
-        assert len(result_dict["elements"]) == 3
+        self.assertEqual(len(result_dict["elements"]), 3)
 
     def test_statement_without_children(self):
         idh = ItemDictHelper("de", self.issue_cat_or_dog)
         result_dict = idh.prepare_item_dict_for_attitude(20)
-        assert len(result_dict["elements"]) == 2
+        self.assertEqual(len(result_dict["elements"]), 2)
         for v in result_dict["elements"]:
-            assert "justify/0/" not in v["url"]
+            self.assertNotIn("justify/0/", v["url"])
