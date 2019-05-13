badcc053f62fd7a32c86be4d4e1c492bf7e1c708
Alexander Schneider <alexander@schneider.gg>
2019-05-13 13:55:48 +0200

    Remove dolan easter-egg

--- docker/db/migrations/v1.5.1/501_transfer_owner_to_postgres.sh
+++ docker/db/migrations/v1.5.1/501_transfer_owner_to_postgres.sh
@@ -6,5 +6,4 @@ for tbl in `psql -qAt -U postgres -c "select tablename from pg_tables where sche
 
 dropuser -U postgres --if-exists dbas
 dropuser -U postgres --if-exists writer
-dropuser -U postgres --if-exists dolan
 dropuser -U postgres --if-exists read_only_discussion
