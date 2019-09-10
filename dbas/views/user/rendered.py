29bb180bc4d8d5408aba1902e1591a310a57c9c3
Jan Steimann <jan.steimann@hhu.de>
2019-09-10 08:20:32 +0200

    Revert "Switch template if user is ldap user"
    This reverts commit 1e947030f8f21c7d077430d8d9e90d7e3eba02ff.

--- dbas/views/user/rendered.py
+++ dbas/views/user/rendered.py
@@ -1,7 +1,6 @@
 import logging
 
 from pyramid.httpexceptions import HTTPNotFound
-from pyramid.renderers import render_to_response
 from pyramid.view import view_config
 
 from dbas.database import DBDiscussionSession
@@ -11,7 +10,7 @@ from dbas.handler.user import get_information_of, change_password
 from dbas.helper.decoration import prep_extras_dict
 from dbas.helper.dictionary.main import DictionaryHelper
 from dbas.input_validator import is_integer
-from dbas.lib import escape_string, checks_if_user_is_ldap_user
+from dbas.lib import escape_string
 from dbas.strings.keywords import Keywords as _
 from dbas.strings.translator import Translator
 from dbas.validators.common import check_authentication
@@ -61,7 +60,7 @@ def user(request):
     return prep_dict
 
 
-@view_config(route_name='main_settings', permission='use')
+@view_config(route_name='main_settings', renderer='../../templates/user/settings.pt', permission='use')
 @validate(valid_user, check_authentication, prep_extras_dict)
 def settings(request):
     """
@@ -72,22 +71,18 @@ def settings(request):
     """
     LOG.debug("Show settings %s", request.params)
 
-    renderer = '../../templates/user/settings.pt'
     ui_locales = get_language_from_cookie(request)
     old_pw, new_pw, confirm_pw, message = '', '', '', ''
     success, error = False, False
     db_user: User = request.validated['user']
 
-    if checks_if_user_is_ldap_user(db_user):
-        renderer = '../../templates/user/settings_ldap_user.pt'
-    else:
-        if 'form.passwordchange.submitted' in request.params:
-            old_pw = escape_string(request.params['passwordold'])
-            new_pw = escape_string(request.params['password'])
-            confirm_pw = escape_string(request.params['passwordconfirm'])
+    if 'form.passwordchange.submitted' in request.params:
+        old_pw = escape_string(request.params['passwordold'])
+        new_pw = escape_string(request.params['password'])
+        confirm_pw = escape_string(request.params['passwordconfirm'])
 
-            message, success = change_password(db_user, old_pw, new_pw, confirm_pw, ui_locales)
-            error = not success
+        message, success = change_password(db_user, old_pw, new_pw, confirm_pw, ui_locales)
+        error = not success
 
     settings_dict = DictionaryHelper(ui_locales).prepare_settings_dict(success, old_pw, new_pw, confirm_pw, error,
                                                                        message, db_user, request.application_url,
@@ -97,8 +92,7 @@ def settings(request):
     prep_dict.update({
         'settings': settings_dict
     })
-
-    return render_to_response(renderer, prep_dict, request=request)
+    return prep_dict
 
 
 @view_config(route_name='main_notification', renderer='../../templates/user/notifications.pt', permission='use')
