111f5f49b189aa6aa10d6325a5adf31e84aad0b6
feger <marc.feger@hhu.de>
2020-04-29 18:17:14 +0200

    Add TypeHints for ReviewDuplicate

--- dbas/database/discussion_model.py
+++ dbas/database/discussion_model.py
@@ -1801,7 +1801,7 @@ class ReviewDuplicate(AbstractReviewCase):
     duplicate_statement: Statement = relationship('Statement', foreign_keys=[duplicate_statement_uid])
     original_statement: Statement = relationship('Statement', foreign_keys=[original_statement_uid])
 
-    def __init__(self, detector: User, duplicate_statement: Statement = None, original_statement: 'Statement' = None,
+    def __init__(self, detector: User, duplicate_statement: Statement = None, original_statement: Statement = None,
                  is_executed: bool = False, is_revoked: bool = False):
         """
         Inits a row in current review duplicate table
@@ -1813,11 +1813,11 @@ class ReviewDuplicate(AbstractReviewCase):
         :param is_revoked: Boolean
         """
         self.detector: User = detector
-        self.duplicate_statement = duplicate_statement
-        self.original_statement = original_statement
-        self.timestamp = get_now()
-        self.is_executed = is_executed
-        self.is_revoked = is_revoked
+        self.duplicate_statement: Statement = duplicate_statement
+        self.original_statement: Statement = original_statement
+        self.timestamp: ArrowType = get_now()
+        self.is_executed: bool = is_executed
+        self.is_revoked: bool = is_revoked
 
     def set_executed(self, is_executed: bool):
         """
