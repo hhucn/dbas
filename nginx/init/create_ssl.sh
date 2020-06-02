c5c7666873816d041653a6be41b38548bac95335
Christian Meter <cmeter@googlemail.com>
2020-06-02 12:25:35 +0200

    Delete nginx ssl script

--- nginx/init/create_ssl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-[ -d /etc/nginx/ssl ] || mkdir -p /etc/nginx/ssl
-apk --update add openssl
-DEPLOY_HOST=${DEPLOY_HOST#https://}
-DEPLOY_HOST=${DEPLOY_HOST#http://}
-openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/CN=$DEPLOY_HOST"
