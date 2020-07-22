371c9c9cfccd48bfdb82afd8fc97b5346ab5fb20
jaste110 <jan.steimann@hhu.de>
2020-07-22 09:45:03 +0200

    Replace slug with random hash if discussion is private

--- dbas/views/discussion/json.py
+++ dbas/views/discussion/json.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 from typing import List
 
 from pyramid.request import Request
@@ -279,14 +280,26 @@ def set_correction_of_some_statements(request):
 def create_issue_after_validation(request: Request):
     LOG.debug("Set a new issue: %s", request.json_body)
 
-    db_issue = Issue(title=escape_string(request.validated['title']),
-                     info=escape_string(request.validated['info']),
-                     long_info=escape_string(request.validated['long_info']),
-                     author=request.validated['user'],
-                     is_read_only=request.validated['is_read_only'],
-                     is_private=not request.validated['is_public'],
-                     language=request.validated['lang'])
+    if request.validated['is_public']:
+        db_issue = Issue(title=escape_string(request.validated['title']),
+                         info=escape_string(request.validated['info']),
+                         long_info=escape_string(request.validated['long_info']),
+                         author=request.validated['user'],
+                         is_read_only=request.validated['is_read_only'],
+                         is_private=not request.validated['is_public'],
+                         language=request.validated['lang'])
+    else:
+        db_issue = Issue(title=escape_string(request.validated['title']),
+                         info=escape_string(request.validated['info']),
+                         long_info=escape_string(request.validated['long_info']),
+                         author=request.validated['user'],
+                         is_read_only=request.validated['is_read_only'],
+                         is_private=not request.validated['is_public'],
+                         language=request.validated['lang'],
+                         slug=uuid.uuid4().hex)
+
     DBDiscussionSession.add(db_issue)
+
     return {'issue': get_issue_dict_for(db_issue, 0, request.validated['lang'].ui_locales)}
 
 
